61b89f2bd2b0a3549c4e48dd73b57fa5bd686fe0
Matt Sheehan <matt@mattsheehan.dev>
2022-04-24 00:12:41 -0400

    change import

--- pokemon/helpers.py
+++ pokemon/helpers.py
@@ -1,5 +1,5 @@
 import pokebase as pb
-import config as config
+from config import *
 import random
 import math
 
@@ -10,7 +10,7 @@ def getStarterPokemon(username):
         if 'cactitwig' in username.lower():
             return {'rattata': 19}
         else:
-            sequence=[{'bulbasaur': 1}, {'charmander': 4}, {'squirtle': 7}]
+            sequence = [{'bulbasaur': 1}, {'charmander': 4}, {'squirtle': 7}]
             starter = random.choice(sequence)
             print(starter)
             return starter
@@ -24,11 +24,11 @@ def getStarterPokemon(username):
 
 
 def expGain(baseExp, level):
-    a = 1 # 1 if wild, 1.5 if owned by trainer
-    b = baseExp # base exp of fainted pokemon
-    L = level # level of fainted pokemon
-    s = 1 # number of participating pokemon
-    t = 1 # 1 if pokemon is current owner, 1.5 if pokemon was gained in a trade
+    a = 1  # 1 if wild, 1.5 if owned by trainer
+    b = baseExp  # base exp of fainted pokemon
+    L = level  # level of fainted pokemon
+    s = 1  # number of participating pokemon
+    t = 1  # 1 if pokemon is current owner, 1.5 if pokemon was gained in a trade
     exp = (a*t*b*L) / (7 * s)
     return exp
 
@@ -39,18 +39,7 @@ def getEffortValue(id_or_name):
     pokemon = pb.pokemon(id_or_name)
     for stat in pokemon.stats:
         statName = stat.stat.name
-        effortValue = stat.effort * config.overallExperienceModifier
+        effortValue = stat.effort * overallExperienceModifier
         effortValueDict[statName] = effortValue
-    
+
     return effortValueDict
-
-
-
-
-
-
-
-
-
-
-
