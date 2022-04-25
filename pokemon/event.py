36b1a51c609e47df1f0f3aec9e9722a4dbed0820
Matt Sheehan <matt@mattsheehan.dev>
2022-04-24 21:48:30 -0400

    test pokemon navigation

--- pokemon/event.py
+++ pokemon/event.py
@@ -66,6 +66,46 @@ class EventMixin(MixinMeta):
                     name="Height", value=f"{pokemon.height}", inline=True)
                 embed.set_thumbnail(url=f"{sprite.url}")
 
+                trainerPokemon['index'] = nextIdx
+
                 await reaction.message.edit(embed=embed)
 
-        pass
+        # TODO: copypasta
+        if reactionId == arrow_backwards:
+            # TODO: don't store these credentials in source control,
+            #       eventually just pass them in as part of the cog config
+            conn = pg.connect(
+                host="REDACTED_HOST",
+                dbname="pokemon_db",
+                user="redbot",
+                password="REDACTED_PASSWORD",
+                port=REDACTED_PORT)
+
+            # TODO: there is a much better way to do this, still playing
+            cur = conn.cursor()
+
+            cur.execute(
+                'select * from trainer_pokemon where trainer_id = %(trainer)s', {'trainer': trainerPokemon['trainer_id']})
+
+            pokemon = cur.fetchall()
+
+            nextIdx = trainerPokemon['index'] - 1
+            if nextIdx <= len(pokemon) - 1:
+                nextPokemon = pokemon[nextIdx]
+
+                name = nextPokemon[1]
+                pokemon = pb.pokemon(name)
+                sprite = pb.SpriteResource('pokemon', pokemon.id)
+
+                embed = discord.Embed(title=f"#{pokemon.id} {pokemon.name}")
+                embed.set_author(name=f"{user.display_name}",
+                                 icon_url=str(user.avatar_url))
+                embed.add_field(
+                    name="Weight", value=f"{pokemon.weight}", inline=True)
+                embed.add_field(
+                    name="Height", value=f"{pokemon.height}", inline=True)
+                embed.set_thumbnail(url=f"{sprite.url}")
+
+                trainerPokemon['index'] = nextIdx
+
+                await reaction.message.edit(embed=embed)
