2cc45674fcc4afc89ba51f025ee30009e19f0812
Matt Sheehan <matt@mattsheehan.dev>
2022-04-25 22:54:38 -0400

    test

--- pokemon/__init__.py
+++ pokemon/__init__.py
@@ -5,9 +5,7 @@ if TYPE_CHECKING:
     from redbot.core.bot import Red
 
 from .main import Pokemon
-from discord_components import DiscordComponents
 
 
 def setup(bot: Red):
-    DiscordComponents(bot)
     bot.add_cog(Pokemon(bot))
--- pokemon/main.py
+++ pokemon/main.py
@@ -23,6 +23,9 @@ import pokebase as pb
 import psycopg as pg
 from .helpers import *
 
+client = commands.Bot('*')
+DiscordComponents(client)
+
 
 class CompositeClass(commands.CogMeta, ABCMeta):
     __slots__: tuple = ()
