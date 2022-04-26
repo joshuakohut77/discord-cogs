2193bdefddf27a9ee2e7d0f63a73402cb4731267
Matt Sheehan <matt@mattsheehan.dev>
2022-04-25 23:06:33 -0400

    test

--- pokemon/__init__.py
+++ pokemon/__init__.py
@@ -1,11 +1,20 @@
 from __future__ import annotations
 from typing import TYPE_CHECKING
 
+import discord
+
 if TYPE_CHECKING:
     from redbot.core.bot import Red
 
 from .main import Pokemon
 
+from discord_components import DiscordComponents
+import discord.ext.commands
+
 
 def setup(bot: Red):
+    if not isinstance(discord.ext.commands.Bot, bot):
+        raise 'incorrect bot type'
+
+    DiscordComponents(bot)
     bot.add_cog(Pokemon(bot))
