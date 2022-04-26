967db7b7a4bae9d3c2554878739aa73da59d1f7c
joshuakohut77 <64916852+joshuakohut77@users.noreply.github.com>
2022-04-26 01:03:02 -0400

    Update pokedexclass.py
    Fixed pokedex query

--- pokemon/pokedexclass.py
+++ pokemon/pokedexclass.py
@@ -14,21 +14,17 @@ class pokedex:
 
     def __pokedex(self):
         """ will update the database with information on the pokemon """
-        now = datetime.now()
+        now = str(datetime.now())
         db = dbconn()
-        updateQuery = """
-            IF EXISTS
-                (SELECT 1 FROM pokedex WHERE "discord_id"=%s AND "pokemonId"=%s)
-            THEN
-                UPDATE pokedex 
-                SET "mostRecent"=%s
-                WHERE "discord_id"=%s AND "pokemonId"=%s
-            ELSE
-                INSERT INTO pokedex ("discord_id", "pokemonId", "pokemonName", "mostRecent")
-                    VALUES (%s, %s, %s, %s)
-            END IF;
-        """
-        values = (self.discordId, self.pokemon.id, now, self.discordId, self.pokemon.id, self.discordId, self.pokemon.id, self.pokemon.name, now)
+        queryString = 'SELECT 1 FROM pokedex WHERE "discord_id"=%s AND "pokemonId"=%s'
+        results = db.runQuery(queryString, (self.discordId, self.pokemon.id))
+        if len(results):
+            updateQuery = 'UPDATE pokedex SET "mostRecent"=%s WHERE "discord_id"=%s AND "pokemonId"=%s'
+            values = (now, self.discordId, str(self.pokemon.id))
+        else:
+            updateQuery = 'INSERT INTO pokedex ("discord_id", "pokemonId", "pokemonName", "mostRecent") VALUES (%s, %s, %s, %s)'
+            values = (self.discordId, str(self.pokemon.id), self.pokemon.name, now)
+        
         db.runUpdateQuery(updateQuery, values)
 
         # delete and close connection
