c3db88803302cf84171f433860740ccb878b08ed
Matt Sheehan <matt@mattsheehan.dev>
2022-04-26 23:26:02 -0400

    start integration

--- pokemon/dbclass.py
+++ pokemon/dbclass.py
@@ -5,14 +5,24 @@ import psycopg2 as pg
 
 class db:
     def __init__(self, params=None):
+        # TODO: need a better way to pass in db configs through all the objects.
         self.conn = pg.connect(
             host=(
-                params and params.host) or "192.168.5.10",
+                params and params.host) or "private-db-redbot-nyc3-42069-do-user-1692759-0.b.db.ondigitalocean.com",
             dbname=(params and params.dbname) or "pokemon_db",
             user=(params and params.user) or "redbot",
             # todo remove password from source control
-            password=(params and params.password) or "bfFLG9tUYPpW7272vzhX52",
-            port=(params and params.port) or 5432)
+            password=(params and params.password) or "AVNS_nUSNiHJE3MlXGmj",
+            port=(params and params.port) or 25060)
+
+        # self.conn = pg.connect(
+        #     host=(
+        #         params and params.host) or "192.168.5.10",
+        #     dbname=(params and params.dbname) or "pokemon_db",
+        #     user=(params and params.user) or "redbot",
+        #     # todo remove password from source control
+        #     password=(params and params.password) or "bfFLG9tUYPpW7272vzhX52",
+        #     port=(params and params.port) or 5432)
 
     def __del__(self):
         self.conn.close()
