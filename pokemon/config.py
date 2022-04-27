2a50b6e1bee8c9ec5f38b79254fcc7a8aa5df5ef
joshuakohut77 <64916852+joshuakohut77@users.noreply.github.com>
2022-04-27 19:08:49 -0400

    Update config.py
    removed prices config as it exists in database

--- pokemon/config.py
+++ pokemon/config.py
@@ -1,18 +1,5 @@
 """ This file is used to store config values for different stats, items, and percent modifiers """
 
-prices = {
-        'balls':[
-                {'pokeball': 100},
-                {'greatball': 600},
-                {'ultraball': 1200}
-            ]
-        ,
-        'buffs':[
-                {'potion': 200}
-            ]
-        ,
-        'pokecenter': 250
-    }
 
 dailyCurrency = 300 # increases with level
 dailyCurrencyLevelModifier = 0.25
