c75ef66e66bf347488109441a2d60faaf23bf7d6
Matt Sheehan <matt@mattsheehan.dev>
2022-04-27 23:21:28 -0400

    experiment

--- pokemon/event.py
+++ pokemon/event.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 from typing import TYPE_CHECKING
-from .abc import MixinMeta
+from abc import MixinMeta
+# import abc
 
 # if TYPE_CHECKING:
 #     import discord
