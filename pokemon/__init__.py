805295babaa16b6cf59a3c1b17a1e8fff8f422c3
Matt Sheehan <matt@mattsheehan.dev>
2022-04-27 22:51:54 -0400

    attempt to append path

--- pokemon/__init__.py
+++ pokemon/__init__.py
@@ -1,12 +1,15 @@
+import sys
+import os
+
 from __future__ import annotations
 from typing import TYPE_CHECKING
-
-import discord
 
 if TYPE_CHECKING:
     from redbot.core.bot import Red
 
 from .main import Pokemon
+
+sys.path.append(os.path.dirname(os.path.realpath(__file__)))
 
 
 def setup(bot: Red):
