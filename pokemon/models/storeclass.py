066672b25c049961696c940935d9a4054bd90bc9
Matt Sheehan <matt@mattsheehan.dev>
2022-04-30 02:02:53 -0400

    test

--- pokemon/models/storeclass.py
+++ pokemon/models/storeclass.py
@@ -8,6 +8,7 @@ class store:
     def __init__(self, discordId):
         self.discordId = discordId
         self.storeList = []
+        self.storeMap = {}
         self.__loadStore()
 
     def __loadStore(self):
@@ -24,13 +25,41 @@ class store:
             storeOption = {'item': item, 'price': price, 'spriteUrl': self.__getSpriteUrl(item)}
             storeList.append(storeOption)
 
+        self.storeMap = {}
+        for item in storeList:
+            if item['item'] in self.storeMap.keys():
+                self.storeMap[item['item']]['details'] = {price: item['price']}
+                
+
         # delete and close connection
         del db
         self.storeList = storeList
+
+    def buyItem2(self, name, quantity):
+        if name not in self.storeMap.keys():
+            return "Item not available"
+        
+        price = self.storeMap[name]['price']
+        totalPrice = price * quantity
+
+        inventory = inv(self.discordId)
+
+        if inventory.money < totalPrice:
+            return 'You do not have enough money to buy that.'
+        else:
+            inventory.money = inventory.money - totalPrice
+            # todo update this so it's not hard coded
+            if name == 'poke-ball':
+                inventory.pokeball = inventory.pokeball + quantity
+            elif name == 'potion':
+                inventory.potion = inventory.potion + quantity
+            inventory.save()
+            return "You successfully bought that item!"
     
     def buyItem(self, itemId, quantity):
         """ buy and item and update trainers inventory """
         inventory = inv(self.discordId)
+
         for item in self.storeList:
             if item['item'] == itemId:
                 if inventory.money < (item['price'] * quantity):
