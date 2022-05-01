21ddb072d5672b6da88f83438c9623c74ba32249
joshuakohut77 <64916852+joshuakohut77@users.noreply.github.com>
2022-05-01 01:16:40 -0400

    Update inventory.sql
    Added all items in game

--- pokemon/sql/inventory.sql
+++ pokemon/sql/inventory.sql
@@ -5,9 +5,26 @@
 CREATE TABLE IF NOT EXISTS public.inventory
 (
     discord_id character varying COLLATE pg_catalog."default" NOT NULL,
-    potion integer DEFAULT 0,
-    "poke-ball" integer DEFAULT 0,
     money integer DEFAULT 0,
+    "poke-ball" integer DEFAULT 0,
+    "great-ball" integer DEFAULT 0,
+    "ultra-ball" integer DEFAULT 0,
+    "master-ball" integer DEFAULT 0,
+    potion integer DEFAULT 0,
+    "super-potion" integer DEFAULT 0,
+    "hyper-potion" integer DEFAULT 0,
+    "max-potion" integer DEFAULT 0,
+    revive integer DEFAULT 0,
+    "full-restore" integer DEFAULT 0,
+    repel integer DEFAULT 0,
+    "max-repel" integer DEFAULT 0,
+    awakening integer DEFAULT 0,
+    "escape-rope" integer DEFAULT 0,
+    "full-heal" integer DEFAULT 0,
+    "ice-heal" integer DEFAULT 0,
+    "burn-heal" integer DEFAULT 0,
+    "paralyze-heal" integer DEFAULT 0,
+    antidote integer DEFAULT 0,
     CONSTRAINT inventory_pkey PRIMARY KEY (discord_id)
 )
 
