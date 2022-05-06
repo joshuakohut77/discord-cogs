116deda3a04f6db491a6bb2a21e3914a9f081278
joshuakohut77 <64916852+joshuakohut77@users.noreply.github.com>
2022-05-06 16:20:36 -0400

    Create loggerclass.py
    Added logger class for detailed traceback logging

---
+++ pokemon/models/loggerclass.py
@@ -0,0 +1,40 @@
+import sys
+import traceback
+import inspect
+
+#------------------------------------------
+#		Josh Kohut / John Hanna / Francis Rainville (Grantek) 2020-07-08
+#		Description: Logger Object
+#------------------------------------------
+class logger:
+				
+    # def __init__(self):  
+
+
+    def error(self, excInfo=None, msg="", verbose=True):
+        """ prints out error messages """
+        errorMsg = '' 
+        # Process the excInfo
+        if excInfo:
+            exc_type, exc_value, exc_traceback = excInfo										
+            errorMsg = exc_value			
+            errorMsg = '%s\nType:%s' % (errorMsg, str(exc_type).replace("<type '", "").replace("'>", "")	)
+            formattedError = traceback.format_exception(exc_type, exc_value, exc_traceback)														
+            for x in range (1, len(formattedError) -1):
+                if formattedError[x].strip()[-12:] == ' in <module>':					
+                    errorMsg = '%s\n%s' % (errorMsg, formattedError[x].strip()[:-12])
+                else:
+                    errorMsg = '%s\n%s' % (errorMsg, formattedError[x].strip())									
+            
+        # Add custom message
+        if msg:
+            if errorMsg:
+                errorMsg = '%s\nMessage:%s' % (errorMsg, msg)
+            else:	
+                errorMsg = msg						
+        
+        if verbose:
+            print(errorMsg)
+
+        return errorMsg 
+
