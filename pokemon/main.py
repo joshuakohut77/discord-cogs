b05d57c5c8bc8703ba0acb38331001512eb13907
Matt Sheehan <matt@mattsheehan.dev>
2022-05-08 22:08:08 -0400

    add pc mixin

--- pokemon/main.py
+++ pokemon/main.py
@@ -20,6 +20,7 @@ import asyncio
 from .functions import (createPokemonAboutEmbed)
 from .starter import StarterMixin
 from .pokemart import PokemartMixin
+from .pc import PcMixin
 # from .event import EventMixin
 
 # import pokebase as pb
@@ -41,7 +42,7 @@ class CompositeClass(commands.CogMeta, ABCMeta):
     pass
 
 
-class Pokemon(StarterMixin, PokemartMixin, commands.Cog, metaclass=CompositeClass):
+class Pokemon(StarterMixin, PcMixin, PokemartMixin, commands.Cog, metaclass=CompositeClass):
     """Pokemon"""
 
     def __init__(self, bot: Red):
@@ -308,152 +309,3 @@ class Pokemon(StarterMixin, PokemartMixin, commands.Cog, metaclass=CompositeClas
         
         # interaction = await self.bot.wait_for("button_click", check=nextBtnClick())
         # await ctx.send(embed=embed)
-
-    # @_trainer.command()
-    # async def starter(self, ctx: commands.Context, user: discord.Member = None) -> None:
-    #     """Show the starter pokemon for the trainer."""
-    #     if user is None:
-    #         user = ctx.author
-
-    #     # This will create the trainer if it doesn't exist
-    #     trainer = TrainerClass(str(user.id))
-    #     pokemon = trainer.getStarterPokemon()
-    #     active = trainer.getActivePokemon()
-
-    #     embed = createPokemonAboutEmbed(user, pokemon)
-
-    #     btns = []
-        
-    #     # btns.append(self.client.add_callback(
-    #     #     Button(style=ButtonStyle.green, label="Stats", custom_id='stats'),
-    #     #     self.on_stats_click,
-    #     # ))
-    #     btns.append(Button(style=ButtonStyle.green, label="Stats", custom_id='stats'))
-    #     btns.append(Button(style=ButtonStyle.green, label="Pokedex", custom_id='pokedex'))
-        
-    #     # Disable the "Set Active" button if the starter is currently the active pokemon
-    #     disabled = (active is not None) and (pokemon.trainerId == active.trainerId)
-    #     btns.append(Button(style=ButtonStyle.blue, label="Set Active", custom_id='active', disabled=disabled))
-
-    #     await ctx.send(embed=embed, components=[btns])
-    #     # await ctx.send(pokemon.frontSpriteURL)
-
-    # async def on_stats_click(self, interaction: Interaction):
-    #     await interaction.send('stats clicked')
-
-    # @_trainer.command()
-    # async def active(self, ctx: commands.Context, user: discord.Member = None) -> None:
-    #     """Show the currect active pokemon for the trainer."""
-    #     if user is None:
-    #         user = ctx.author
-
-    #      # This will create the trainer if it doesn't exist
-    #     trainer = TrainerClass(str(user.id))
-    #     pokemon = trainer.getActivePokemon()
-
-    #     btns = []
-    #     btns.append(Button(style=ButtonStyle.green, label="Stats", custom_id='stats'))
-    #     btns.append(Button(style=ButtonStyle.green, label="Pokedex", custom_id='pokedex'))
-
-    #     embed = createPokemonAboutEmbed(user, pokemon)
-    #     await ctx.send(embed=embed)       
-
-
-    # @_trainer.command()
-    # async def pokemon(self, ctx: commands.Context, user: discord.Member = None) -> None:
-    #     """Show the starter pokemon for the trainer.
-    #     """
-    #     if user is None:
-    #         user = ctx.author
-
-    #     # TODO: don't store these credentials in source control,
-    #     #       eventually just pass them in as part of the cog config
-    #     conn = pg.connect(
-    #         host="private-db-redbot-nyc3-42069-do-user-1692759-0.b.db.ondigitalocean.com",
-    #         dbname="pokemon_db",
-    #         user="redbot",
-    #         password="AVNS_nUSNiHJE3MlXGmj",
-    #         port=25060)
-
-    #     # TODO: there is a much better way to do this, still playing
-    #     cur = conn.cursor()
-    #     cur.execute(
-    #         'select * from trainer where discord_id = %(discord)s', {'discord': user.id})
-
-    #     trainer = cur.fetchone()
-
-    #     if trainer is None:
-    #         await ctx.send('You haven\'t received your started yet!')
-
-    #     cur.execute(
-    #         'select * from trainer_pokemon where trainer_id = %(trainer)s', {'trainer': trainer[0]})
-
-    #     pokemon = cur.fetchall()
-
-    #     if len(pokemon) == 0:
-    #         await ctx.send('You haven\'t received your started yet!')
-
-    #     cur.close()
-    #     conn.close()
-
-    #     firstPokemon = pokemon[0]
-    #     # TODO: replace with pokeclass to calculate unique stats per pokemon
-    #     name = firstPokemon[1]
-    #     pokemon = pb.pokemon(name)
-    #     sprite = pb.SpriteResource('pokemon', pokemon.id)
-
-    #     # Create the embed object
-    #     embed = discord.Embed(title=f"#{pokemon.id} {pokemon.name}")
-    #     embed.set_author(name=f"{user.display_name}",
-    #                      icon_url=str(user.avatar_url))
-    #     embed.add_field(name="Weight", value=f"{pokemon.weight}", inline=True)
-    #     embed.add_field(name="Height", value=f"{pokemon.height}", inline=True)
-    #     embed.set_thumbnail(url=f"{sprite.url}")
-
-    #     msg: discord.Message = await ctx.send(embed=embed)
-
-    #     self.pokelist[f'{user.id}'] = {'message_id': msg.id,
-    #                                    'trainer_id': trainer[0], 'index': 0}
-
-    #     # emoji: discord.Emoji = await commands.EmojiConverter().convert(ctx=await self.bot.get_context(msg), argument=':arrow_backward:')
-    #     # if emoji is None:
-    #     #     await msg.reply('emoji null')
-    #     # emoji = self.bot.get_emoji()
-    #     await msg.add_reaction('◀️')
-    #     await msg.add_reaction('▶️')
-
-    #     # await msg.reply('done')
-    #     await ctx.tick()
-
-    # @_trainer.command()
-    # async def button(self, ctx: commands.Context, user: discord.Member = None) -> None:
-    #     """Test button
-    #     """
-    #     # await self.ui.components.send(ctx.channel, "Hello World", components=[
-    #     #     Button("press me", "my_custom_id", "green"),
-    #     # ])
-
-    #     # async def callback(interaction):
-    #     #     await interaction.send(content="Yay")
-
-    #     btn = Button(style=ButtonStyle.gray,
-    #                  label="Button 1", custom_id='button1')
-    #     btn2 = Button(style=ButtonStyle.gray,
-    #                   label="Button 2", custom_id='button2')
-
-    #     await ctx.send(
-    #         "Buttons",
-    #         components=[[
-    #             btn, btn2
-    #             # self.bot.components_manager.add_callback(b, callback)
-    #         ]]
-    #     )
-
-    #     interaction = await self.bot.wait_for("button_click", check=lambda i: i.custom_id == "button1")
-    #     # await interaction.message.edit('Buttons', components=[])
-    #     await interaction.edit_origin('Buttons', components=[])
-    #     # await interaction.send('Button 1 clicked')
-    #     # await interaction.send('Done')
-    #     # await interaction.send(f'{msg.id}')
-
-    #     # msg.edit('Buttons')
