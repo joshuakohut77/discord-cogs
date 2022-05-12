9ad750db10c5d38d7a18aafba9fd95788939e295
Matt Sheehan <matt@mattsheehan.dev>
2022-05-11 22:07:58 -0400

    update map CBs

--- pokemon/map.py
+++ pokemon/map.py
@@ -8,12 +8,15 @@ from discord import message
 from discord_components import (
     DiscordComponents, ButtonStyle, ComponentsBot, Button, Interaction)
 
+
 if TYPE_CHECKING:
     from redbot.core.bot import Red
 
 from redbot.core import commands
 
+from models.location import LocationModel
 from services.trainerclass import trainer as TrainerClass
+from services.locationclass import location as LocationClass
 
 
 from .abcd import MixinMeta
@@ -22,9 +25,21 @@ from .functions import (createStatsEmbed, getTypeColor,
                         createPokemonAboutEmbed)
 
 
+class LocationState:
+    discordId: str
+    location: LocationModel
+    messageId: int
+
+    def __init__(self, discordId: str, location: LocationModel, messageId: int) -> None:
+        self.discordId = discordId
+        self.location = location
+        self.messageId = messageId
+
+
 class MapMixin(MixinMeta):
     """Map"""
 
+    __locations: dict[str, LocationState] = {}
 
     @commands.group(name="trainer")
     @commands.guild_only()
@@ -45,18 +60,123 @@ class MapMixin(MixinMeta):
 
         btns = []
         if location.north is not None:
-            btns.append(Button(style=ButtonStyle.gray, emoji='⬆' label=f"{location.north}"))
+            btns.append(self.client.add_callback(
+                Button(style=ButtonStyle.gray, emoji='⬆', label=f"{location.north}"),
+                self.on_north,
+            ))
         if location.south is not None:
-            btns.append(Button(style=ButtonStyle.gray, emoji='⬇' label=f"{location.south}"))
+            btns.append(self.client.add_callback(
+                Button(style=ButtonStyle.gray, emoji='⬇', label=f"{location.south}"),
+                self.on_south,
+            ))
         if location.east is not None:
+            btns.append(self.client.add_callback(
+                Button(style=ButtonStyle.gray, emoji='➡', label=f"{location.east}"),
+                self.on_east,
+            ))
             # emote: discord.Emoji = await commands.EmojiConverter().convert(ctx=ctx, argument='⬅️')
-            btns.append(Button(style=ButtonStyle.gray, emoji='➡', label=f"{location.east}"))
         if location.west is not None:
+            btns.append(self.client.add_callback(
+                Button(style=ButtonStyle.gray, emoji='⬅', label=f"{location.west}"),
+                self.on_west,
+            ))
             # emote: discord.Emoji = await commands.EmojiConverter().convert(ctx=ctx, argument='⬅️')
-            btns.append(Button(style=ButtonStyle.gray, emoji='⬅', label=f"{location.west}"))
 
-        await ctx.send(
+        message = await ctx.send(
             content=location.name,
             file=file,
             components=[btns]
         )
+        self.__locations[str(user.id)] = LocationState(str(user.id), location, message.id)
+
+    async def on_north(self, interaction: Interaction):
+        user = interaction.user
+
+        if not self.__checkTrainerState(user, interaction.message):
+            await interaction.send('This is not for you.')
+            return
+
+        state = self.__locations[str(user.id)]
+        north = state.location.north
+
+        loc = LocationClass()
+        direction = loc.getLocationByName(north)
+        if loc.statuscode == 96:
+            await interaction.send(loc.message)
+            return
+
+        trainer = TrainerClass(str(user.id))
+        trainer.setLocation(direction.locationId)
+        await interaction.respond(f'You walked to {north}')
+        
+
+    async def on_south(self, interaction: Interaction):
+        user = interaction.user
+
+        if not self.__checkTrainerState(user, interaction.message):
+            await interaction.send('This is not for you.')
+            return
+
+        state = self.__locations[str(user.id)]
+        south = state.location.south
+
+        loc = LocationClass()
+        direction = loc.getLocationByName(south)
+        if loc.statuscode == 96:
+            await interaction.send(loc.message)
+            return
+
+        trainer = TrainerClass(str(user.id))
+        trainer.setLocation(direction.locationId)
+        await interaction.respond(f'You walked to {south}')
+
+    async def on_east(self, interaction: Interaction):
+        user = interaction.user
+
+        if not self.__checkTrainerState(user, interaction.message):
+            await interaction.send('This is not for you.')
+            return
+
+        state = self.__locations[str(user.id)]
+        east = state.location.east
+
+        loc = LocationClass()
+        direction = loc.getLocationByName(east)
+        if loc.statuscode == 96:
+            await interaction.send(loc.message)
+            return
+
+        trainer = TrainerClass(str(user.id))
+        trainer.setLocation(direction.locationId)
+        await interaction.respond(f'You walked to {east}')
+
+    async def on_west(self, interaction: Interaction):
+        user = interaction.user
+
+        if not self.__checkTrainerState(user, interaction.message):
+            await interaction.send('This is not for you.')
+            return
+
+        state = self.__locations[str(user.id)]
+        west = state.location.west
+
+        loc = LocationClass()
+        direction = loc.getLocationByName(west)
+        if loc.statuscode == 96:
+            await interaction.send(loc.message)
+            return
+
+        trainer = TrainerClass(str(user.id))
+        trainer.setLocation(direction.locationId)
+        await interaction.respond(f'You walked to {west}')
+
+
+    def __checkTrainerState(self, user: discord.User, message: discord.Message):
+        state: LocationState
+        if str(user.id) not in self.__locations.keys():
+            return False
+        else:
+            state = self.__locations[str(user.id)]
+            if state.messageId != message.id:
+                return False
+        return True
