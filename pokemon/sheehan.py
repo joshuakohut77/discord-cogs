7e54b4779b9d83114fff87ca6f8c98450fe706be
Matt Sheehan <matt@mattsheehan.dev>
2022-05-12 00:23:38 -0400

    encounter fix

--- pokemon/sheehan.py
+++ pokemon/sheehan.py
@@ -7,15 +7,19 @@ sys.path.append(os.path.join(os.path.dirname(
 sys.path.append(os.path.join(os.path.dirname(
     os.path.realpath(__file__)), 'services'))
 
-# from models.trainerclass import trainer as TrainerClass
+from services.trainerclass import trainer as TrainerClass
 from services.locationclass import location as LocationClass
 
-# trainer = TrainerClass('509767223938777108')
+
+trainer = TrainerClass('509767223938777108')
+pokemon = trainer.encounter('walk')
 # areaId = trainer.getAreaId()
 
 # Location: 88 - Kanto Route 1
 # Location Area: 295 - Kanto Route 1 Area
 loc = LocationClass()
 direction = loc.getLocationByName('kanto-route-3')
+
+
 # encounters = loc.getAreaEncounterDetails(295)
 # methods = loc.getMethods(encounters)
