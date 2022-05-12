f920e59595da07bfeb56a781c4c1226a2bd6bac7
Matt Sheehan <matt@mattsheehan.dev>
2022-05-11 22:18:02 -0400

    update

--- pokemon/models/location.py
+++ pokemon/models/location.py
@@ -4,11 +4,11 @@ from typing import List
 class LocationModel:
     
     def __init__(self, results: List):
-        self.locationId: int = results[0]
-        self.name: str = results[1]
-        self.north: str = results[2]
-        self.east: str = results[3]
-        self.south: str = results[4]
-        self.west: str = results[5]
+        self.locationId = results[0]
+        self.name = results[1]
+        self.north = results[2]
+        self.east = results[3]
+        self.south = results[4]
+        self.west = results[5]
         self.prerequisites = results[6]
-        self.spritePath: str = results[7]
+        self.spritePath = results[7]
