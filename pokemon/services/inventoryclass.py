db4e24973bd387e5044c859d53c6e659f4b99b77
joshuakohut77 <64916852+joshuakohut77@users.noreply.github.com>
2022-05-12 23:13:34 -0400

    Update inventoryclass.py
    updated inventory with new items

--- pokemon/services/inventoryclass.py
+++ pokemon/services/inventoryclass.py
@@ -33,6 +33,34 @@ class inventory:
         self.maxpotion = None
         self.antidote = None
         self.superrepel = None
+        self.calcium = None
+        self.carbos = None
+        self.coincase = None
+        self.direhit = None
+        self.domefossil = None
+        self.freshwater = None
+        self.helixfossil = None
+        self.hpup = None
+        self.lemonade = None
+        self.elixer = None
+        self.maxelixer = None
+        self.maxether = None
+        self.ether = None
+        self.nugget = None
+        self.oldamber = None
+        self.pokedoll = None
+        self.ppup = None
+        self.sodapop = None
+        self.townmap = None
+        self.xaccuracy = None
+        self.xdefense = None
+        self.xspatk = None
+        self.xspeed = None
+        self.firestone = None
+        self.waterstone = None
+        self.thunderstone = None
+        self.leafstone = None
+        self.moonstone = None
         self.__loadInventory()
 
     def save(self):
@@ -45,7 +73,17 @@ class inventory:
                                 "repel"=%(repel)s, "awakening"=%(awakening)s, "master-ball"=%(masterball)s,
                                 "escape-rope"=%(escaperope)s, "full-heal"=%(fullheal)s, "ice-heal"=%(iceheal)s, 
                                 "max-repel"=%(maxrepel)s, "burn-heal"=%(burnheal)s, "paralyze-heal"=%(paralyzeheal)s, 
-                                "max-potion"=%(maxpotion)s, "antidote"=%(antidote)s, "super-repel"=%(superrepel)s
+                                "max-potion"=%(maxpotion)s, "antidote"=%(antidote)s, "super-repel"=%(superrepel)s,
+                                "calcium"=%(calcium)s, "carbos"=%(carbos)s, "coin-case"=%(coincase)s,
+                                "dire-hit"=%(direhit)s, "dome-fossil"=%(domefossil)s, "fresh-water"=%(freshwater)s,
+                                "helix-fossil"=%(helixfossil)s, "hp-up"=%(hpup)s, "lemonade"=%(lemonade)s, 
+                                "elixer"=%(elixer)s, "max-elixer"=%(maxelixer)s, "max-ether"=%(maxether)s, 
+                                "ether"=%(ether)s, "nugget"=%(nugget)s, "old-amber"=%(oldamber)s, 
+                                "poke-doll"=%(pokedoll)s, "pp-up"=%(ppup)s, "soda-pop"=%(sodapop)s, 
+                                "town-map"=%(townmap)s, "x-accuracy"=%(xaccuracy)s, "x-defense"=%(xdefense)s, 
+                                "x-sp-atk"=%(xspatk)s, "x-speed"=%(xspeed)s, "fire-stone"=%(firestone)s, 
+                                "water-stone"=%(waterstone)s, "thunder-stone"=%(thunderstone)s, 
+                                "leaf-stone"=%(leafstone)s, "moon-stone"=%(moonstone)s
                                 WHERE "discord_id"=%(discordId)s'''
             values = { 'money': self.money, 'pokeball':self.pokeball,
                             'potion': self.potion, 'greatball': self.greatball, 'ultraball': self.ultraball,
@@ -54,7 +92,17 @@ class inventory:
                             'masterball': self.masterball, 'escaperope': self.escaperope, 'fullheal': self.fullheal, 
                             'iceheal': self.iceheal, 'maxrepel': self.maxrepel, 'burnheal': self.burnheal, 
                             'paralyzeheal': self.paralyzeheal, 'maxpotion': self.maxpotion, 'antidote': self.antidote, 
-                            'superrepel': self.superrepel, 'discordId': self.discordId }
+                            'superrepel': self.superrepel, 'calcium': self.calcium, 'carbos': self.carbos, 
+                            'coincase': self.coincase, 'direhit': self.direhit, 'domefossil': self.domefossil, 
+                            'freshwater': self.freshwater, 'helixfossil': self.helixfossil,
+                            'hpup': self.hpup, 'lemonade': self.lemonade, 
+                            'elixer': self.elixer, 'maxelixer': self.maxelixer, 'maxether': self.maxether, 
+                            'ether': self.ether, 'nugget': self.nugget, 'oldamber': self.oldamber, 
+                            'pokedoll': self.pokedoll, 'ppup': self.ppup, 'sodapop': self.sodapop, 
+                            'townmap': self.townmap, 'xaccuracy': self.xaccuracy, 'xdefense': self.xdefense, 
+                            'xspatk': self.xspatk, 'xspeed': self.xspeed, 'firestone': self.firestone, 
+                            'waterstone': self.waterstone, 'thunderstone': self.thunderstone, 
+                            'leafstone': self.leafstone, 'moonstone': self.moonstone, 'discordId': self.discordId }
             db.execute(updateString, values)
         except:
             self.statuscode = 96
@@ -72,7 +120,13 @@ class inventory:
                             "master-ball", "potion", "super-potion", "hyper-potion", "revive", 
                             "full-restore", "repel", "awakening", "escape-rope", "full-heal",
                             "ice-heal", "max-repel", "burn-heal", "paralyze-heal", 
-                            "max-potion", "antidote", "super-repel" 
+                            "max-potion", "antidote", "super-repel", calcium, carbos, 
+                            "coin-case", "dire-hit", "dome-fossil", "fresh-water", 
+                            "helix-fossil", "hp-up", lemonade, elixer, "max-elixir", 
+                            "max-ether", ether, nugget, "old-amber", "poke-doll", 
+                            "pp-up", "soda-pop", "town-map", "x-accuracy", "x-defense", 
+                            "x-sp-atk", "x-speed", "fire-stone", "water-stone", 
+                            "thunder-stone", "leaf-stone", "moon-stone" 
                             FROM inventory WHERE "discord_id"=%(discordId)s'''
             result = db.querySingle(queryString, { 'discordId': self.discordId })
             if len(result) > 0:
@@ -96,7 +150,35 @@ class inventory:
                 self.paralyzeheal = result[17]
                 self.maxpotion = result[18]
                 self.antidote = result[19]
-                self.antidote = result[20]
+                self.superrepel = result[20]
+                self.calcium = result[21]
+                self.carbos = result[22]
+                self.coincase = result[23]
+                self.direhit = result[24]
+                self.domefossil = result[25]
+                self.freshwater = result[26]
+                self.helixfossil = result[27]
+                self.hpup = result[28]
+                self.lemonade = result[29]
+                self.elixer = result[30]
+                self.maxelixer = result[31]
+                self.maxether = result[32]
+                self.ether = result[33]
+                self.nugget = result[34]
+                self.oldamber = result[35]
+                self.pokedoll = result[36]
+                self.ppup = result[37]
+                self.sodapop = result[38]
+                self.townmap = result[39]
+                self.xaccuracy = result[40]
+                self.xdefense = result[41]
+                self.xspatk = result[42]
+                self.xspeed = result[43]
+                self.firestone = result[44]
+                self.waterstone = result[45]
+                self.thunderstone = result[46]
+                self.leafstone = result[47]
+                self.moonstone = result[48]
         except:
             self.statuscode = 96
             logger.error(excInfo=sys.exc_info())
