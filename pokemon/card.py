1901457bfca799708333e2956b4ff187e3d4a246
Matt Sheehan <matt@mattsheehan.dev>
2022-05-14 23:29:43 -0400

    stats

--- pokemon/card.py
+++ pokemon/card.py
@@ -17,6 +17,7 @@ import constant
 from services.trainerclass import trainer as TrainerClass
 from services.inventoryclass import inventory as InventoryClass
 from services.keyitemsclass import keyitems as KeyItemsClass
+from services.leaderboardclass import leaderboard as LeaderBoardClass
 
 from .abcd import MixinMeta
 from .functions import (createStatsEmbed, getTypeColor,
@@ -56,6 +57,72 @@ class TrainerCardMixin(MixinMeta):
         inventory = InventoryClass(trainer.discordId)
         keyitems = KeyItemsClass(trainer.discordId)
         
+        embed = self.__createAboutEmbed(user, inventory)
+
+        btns = []
+        btns.append(self.client.add_callback(
+            Button(style=ButtonStyle.green, label="Stats", custom_id='stats'),
+            self.on_stats_click,
+        ))
+ 
+        message = await ctx.send(embed=embed, components=[btns])     
+        self.__cards[str(user.id)] = TrainerCardState(str(user.id), message.id)
+
+
+    async def on_stats_click(self, interaction: Interaction):
+        user = interaction.user
+
+        if not self.__checkTrainerState(user, interaction.message):
+            await interaction.send('This is not for you.')
+            return
+
+        stats = LeaderBoardClass(str(user.id))
+
+        embed = discord.Embed(title=f"Trainer")
+        embed.set_author(name=f"{user.display_name}", icon_url=str(user.avatar_url))
+        
+        embed.add_field(name='Battles', value=f'¥{stats.total_battles}', inline=True)
+        embed.add_field(name='Victories', value=f'¥{stats.total_victory}', inline=True)
+        embed.add_field(name='Defeats', value=f'¥{stats.total_defeat}', inline=True)
+        embed.add_field(name='Pokemon Caught', value=f'¥{stats.total_catch}', inline=True)
+        embed.add_field(name='Pokemon Released', value=f'¥{stats.total_released}', inline=True)
+        embed.add_field(name='Pokemon Evolved', value=f'¥{stats.total_evolved}', inline=True)
+        embed.add_field(name='Pokemon Traded', value=f'¥{stats.total_trades}', inline=True)
+
+        btns = []
+        btns.append(self.client.add_callback(
+            Button(style=ButtonStyle.green, label="About", custom_id='about'),
+            self.on_about_click,
+        ))
+ 
+        message = await interaction.edit_origin(embed=embed, components=[btns])     
+        self.__cards[str(user.id)] = TrainerCardState(str(user.id), message.id)
+
+    async def on_about_click(self, interaction: Interaction):
+        user = interaction.user
+
+        if not self.__checkTrainerState(user, interaction.message):
+            await interaction.send('This is not for you.')
+            return
+
+        #  # This will create the trainer if it doesn't exist
+        trainer = TrainerClass(str(user.id))
+        inventory = InventoryClass(trainer.discordId)
+        keyitems = KeyItemsClass(trainer.discordId)
+        
+        embed = self.__createAboutEmbed(user, inventory)
+
+        btns = []
+        btns.append(self.client.add_callback(
+            Button(style=ButtonStyle.green, label="Stats", custom_id='stats'),
+            self.on_stats_click,
+        ))
+ 
+        message = await interaction.edit_origin(embed=embed, components=[btns])     
+        self.__cards[str(user.id)] = TrainerCardState(str(user.id), message.id)
+
+
+    def __createAboutEmbed(self, user: discord.User, inventory: InventoryClass):
         embed = discord.Embed(title=f"Trainer")
         embed.set_author(name=f"{user.display_name}", icon_url=str(user.avatar_url))
         
@@ -76,33 +143,6 @@ class TrainerCardMixin(MixinMeta):
 
         embed.add_field(name='Pokedex', value='0')
         embed.add_field(name='Started', value='2022/05/14')
-
-        # pokemon = trainer.getActivePokemon()
-
-        # btns = []
-        # btns.append(self.client.add_callback(
-        #     Button(style=ButtonStyle.green, label="Stats", custom_id='stats'),
-        #     self.on_stats_click,
-        # ))
-        # btns.append(Button(style=ButtonStyle.green,
-        #             label="Pokedex", custom_id='pokedex'))
-        # Create the embed object
-
-        
-        # embed.set_thumbnail(url=pokemon.frontSpriteURL)
-            # message = await ctx.send(embed=embed, components=[btns])  
-        message = await ctx.send(embed=embed, components=[])     
-        self.__cards[str(user.id)] = TrainerCardState(str(user.id), message.id)
-
-
-    async def on_click(self, interaction: Interaction):
-        user = interaction.user
-
-        if not self.__checkTrainerState(user, interaction.message):
-            await interaction.send('This is not for you.')
-            return
-
-        await interaction.send('Not implemented yet')
 
 
     def __checkTrainerState(self, user: discord.User, message: discord.Message):
