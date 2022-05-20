190ad59a8e232be45b03805a4e5420245166e0fc
joshuakohut77 <64916852+joshuakohut77@users.noreply.github.com>
2022-05-19 22:53:28 -0400

    Update quests.py
    added quest place holders for all locations

--- pokemon/configs/quests.py
+++ pokemon/configs/quests.py
@@ -1,7 +1,248 @@
+
 questConfig = {
-    86: {
-                'pre-requsites': ['oaks_parcel'],
-                'quest': 'Professor Oak',
-                'blockers': []
-        } 
-}
+        67: {
+                        'pre-requsites': [],
+                        'quest': '',
+                        'blockers': []
+                },
+        68: {
+                        'pre-requsites': [],
+                        'quest': '',
+                        'blockers': []
+                },
+        71: {
+                        'pre-requsites': [],
+                        'quest': '',
+                        'blockers': []
+                },
+        73: {
+                        'pre-requsites': [],
+                        'quest': '',
+                        'blockers': []
+                },
+        76: {
+                        'pre-requsites': [],
+                        'quest': '',
+                        'blockers': []
+                },
+        80: {
+                        'pre-requsites': [],
+                        'quest': '',
+                        'blockers': []
+                },
+        86: {
+                        'pre-requsites': ['oaks_parcel'],
+                        'quest': 'Professor Oak',
+                        'blockers': []
+                },
+        87: {
+                        'pre-requsites': [],
+                        'quest': '',
+                        'blockers': []
+                },
+        88: {
+                        'pre-requsites': [],
+                        'quest': '',
+                        'blockers': []
+                },
+        89: {
+                        'pre-requsites': [],
+                        'quest': '',
+                        'blockers': []
+                },
+        90: {
+                        'pre-requsites': [],
+                        'quest': '',
+                        'blockers': []
+                },
+        91: {
+                        'pre-requsites': [],
+                        'quest': '',
+                        'blockers': []
+                },
+        92: {
+                        'pre-requsites': [],
+                        'quest': '',
+                        'blockers': []
+                },
+        93: {
+                        'pre-requsites': [],
+                        'quest': '',
+                        'blockers': []
+                },
+        94: {
+                        'pre-requsites': [],
+                        'quest': '',
+                        'blockers': []
+                },
+        95: {
+                        'pre-requsites': [],
+                        'quest': '',
+                        'blockers': []
+                },	
+        96: {
+                        'pre-requsites': [],
+                        'quest': '',
+                        'blockers': []
+                },
+        97: {
+                        'pre-requsites': [],
+                        'quest': '',
+                        'blockers': []
+                },
+        98: {
+                        'pre-requsites': [],
+                        'quest': '',
+                        'blockers': []
+                },
+        99: {
+                        'pre-requsites': [],
+                        'quest': '',
+                        'blockers': []
+                },
+        100: {
+                        'pre-requsites': [],
+                        'quest': '',
+                        'blockers': []
+                },		
+        101: {
+                        'pre-requsites': [],
+                        'quest': '',
+                        'blockers': []
+                },		
+        102: {
+                        'pre-requsites': [],
+                        'quest': '',
+                        'blockers': []
+                },		
+        103: {
+                        'pre-requsites': [],
+                        'quest': '',
+                        'blockers': []
+                },		
+        104: {
+                        'pre-requsites': [],
+                        'quest': '',
+                        'blockers': []
+                },		
+        109: {
+                        'pre-requsites': [],
+                        'quest': '',
+                        'blockers': []
+                },		
+        120: {
+                        'pre-requsites': [],
+                        'quest': '',
+                        'blockers': []
+                },		
+        130: {
+                        'pre-requsites': [],
+                        'quest': '',
+                        'blockers': []
+                },		
+        131: {
+                        'pre-requsites': [],
+                        'quest': '',
+                        'blockers': []
+                },		
+        132: {
+                        'pre-requsites': [],
+                        'quest': '',
+                        'blockers': []
+                },		
+        133: {
+                        'pre-requsites': [],
+                        'quest': '',
+                        'blockers': []
+                },		
+        134: {
+                        'pre-requsites': [],
+                        'quest': '',
+                        'blockers': []
+                },		
+        136: {
+                        'pre-requsites': [],
+                        'quest': '',
+                        'blockers': []
+                },												
+        147: {
+                        'pre-requsites': [],
+                        'quest': '',
+                        'blockers': []
+                },		
+        151: {
+                        'pre-requsites': [],
+                        'quest': '',
+                        'blockers': []
+                },		
+        152: {
+                        'pre-requsites': [],
+                        'quest': '',
+                        'blockers': []
+                },		
+        154: {
+                        'pre-requsites': [],
+                        'quest': '',
+                        'blockers': []
+                },		
+        155: {
+                        'pre-requsites': [],
+                        'quest': '',
+                        'blockers': []
+                },		
+        157: {
+                        'pre-requsites': [],
+                        'quest': '',
+                        'blockers': []
+                },		
+        158: {
+                        'pre-requsites': [],
+                        'quest': '',
+                        'blockers': []
+                },		
+        159: {
+                        'pre-requsites': [],
+                        'quest': '',
+                        'blockers': []
+                },	
+        160: {
+                        'pre-requsites': [],
+                        'quest': '',
+                        'blockers': []
+                },	
+        161: {
+                        'pre-requsites': [],
+                        'quest': '',
+                        'blockers': []
+                },	
+        162: {
+                        'pre-requsites': [],
+                        'quest': '',
+                        'blockers': []
+                },		
+        231: {
+                        'pre-requsites': [],
+                        'quest': '',
+                        'blockers': []
+                },		
+        232: {
+                        'pre-requsites': [],
+                        'quest': '',
+                        'blockers': []
+                },		
+        233: {
+                        'pre-requsites': [],
+                        'quest': '',
+                        'blockers': []
+                },		
+        234: {
+                        'pre-requsites': [],
+                        'quest': '',
+                        'blockers': []
+                },		
+        498: {
+                        'pre-requsites': [],
+                        'quest': '',
+                        'blockers': []
+                }
+}        
