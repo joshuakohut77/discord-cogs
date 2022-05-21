3347b25a1c8945592374bba1378930bf7549a081
Matt Sheehan <matt@mattsheehan.dev>
2022-05-21 01:39:41 -0400

    set_image

--- pokemon/encounters.py
+++ pokemon/encounters.py
@@ -312,7 +312,7 @@ class EncountersMixin(MixinMeta):
             name="HP", value=f"{pokemon.currentHP} / {stats['hp']}", inline=False)
 
         embed.set_thumbnail(url=pokemon.frontSpriteURL)
-        embed.set_image(pokemon.frontSpriteURL)
+        embed.set_image(url = pokemon.frontSpriteURL)
         return embed
 
 
