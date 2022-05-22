9af7286747730c2c8156e62b0c52f739b82ae0d9
Matt Sheehan <matt@mattsheehan.dev>
2022-05-22 00:15:06 -0400

    combine fns

--- pokemon/models/state.py
+++ pokemon/models/state.py
@@ -1,17 +1,28 @@
 
-from typing import Any, Dict, List, Union, TYPE_CHECKING
+from typing import List
 from services.pokeclass import Pokemon as PokemonClass
+
+from enum import Enum
+
+
+class DisplayCard(Enum):
+    STATS = 1
+    MOVES = 2
+    DEX = 3
+
 
 class PokemonState:
     discordId: str
     messageId: int
+    card: DisplayCard
     pokemon: List[PokemonClass]
     active: int
     idx: int
 
-    def __init__(self, discordId: str, messageId: int, pokemon: List[PokemonClass], active: int, idx: int) -> None:
+    def __init__(self, discordId: str, messageId: int, card: DisplayCard, pokemon: List[PokemonClass], active: int, idx: int) -> None:
         self.discordId = discordId
         self.messageId = messageId
+        self.card = card
         self.pokemon = pokemon
         self.active = active
         self.idx = idx
