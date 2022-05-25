2577e513d3b6a04b189eb9aeecc1efa0be17a377
Matt Sheehan <matt@mattsheehan.dev>
2022-05-25 00:17:42 -0400

    Trade

--- pokemon/pokecenter.py
+++ pokemon/pokecenter.py
@@ -1,27 +1,26 @@
 from __future__ import annotations
-from re import A
 from typing import Any, Dict, List, Union, TYPE_CHECKING
 
 
 import discord
-from discord import (Embed, Member)
-from discord import message
-from discord_components import (
-    DiscordComponents, ButtonStyle, ComponentsBot, Button, Interaction)
+from discord_components import (ButtonStyle, Button, Interaction)
+
+from main import Pokemon
 
 if TYPE_CHECKING:
     from redbot.core.bot import Red
 
 from redbot.core import commands
 
+import constant
 from services.trainerclass import trainer as TrainerClass
 from services.storeclass import store as StoreClass
-
+from services.pokeclass import Pokemon as PokemonClass
 
 from .abcd import MixinMeta
 from .functions import (createStatsEmbed, getTypeColor,
                         createPokemonAboutEmbed)
-import constant
+
 
 
 class PokecenterMixin(MixinMeta):
@@ -53,19 +52,52 @@ class PokecenterMixin(MixinMeta):
 
 
     async def trade(self, ctx: commands.Context, trainerUser: Union[discord.Member,discord.User], pokemonId: str):
-        user = ctx.author
+        user: discord.User = ctx.author
 
         trader = TrainerClass(user.id)
         pokemon = trader.getPokemonById(pokemonId)
 
         if trader.statuscode == 96:
-            await ctx.send('Trade failed')
+            await ctx.send(f'{user.display_name}\'s trade failed.')
             return
 
 
-        await ctx.send('Other trainer has to accept your trade request')
+        # await ctx.send('Other trainer has to accept your trade request')
 
+        embed, btns = self.__pokemonSingleCard(user, pokemon)
 
+        await ctx.send(
+            content=f'{trainerUser.mention} {user.display_name} wants to trade with you.',
+            embed=embed,
+            components=btns
+
+        )
 
         # tradee = TrainerClass(trainerUser.id)
+
+
+    async def __on_trade_click(self, interaction: Interaction):
+        pass
+
+
+    def __pokemonSingleCard(self, user: discord.User, pokemon: Pokemon):
+
+        embed = createStatsEmbed(user, pokemon)
+
+        firstRowBtns = []
+
+        firstRowBtns.append(self.client.add_callback(
+            Button(style=ButtonStyle.green, label="Accept Trade", custom_id='accept'),
+            self.__on_trade_click
+        ))
+        firstRowBtns.append(self.client.add_callback(
+            Button(style=ButtonStyle.red, label="Decline Trade", custom_id='decline'),
+            self.__on_trade_click
+        ))
+
+        btns = []
+        if len(firstRowBtns) > 0:
+            btns.append(firstRowBtns)
+
+        return embed, btns
 
