2774f1f9d62e020a2a8e95bef64abaeac8bdc576
joshuakohut77 <64916852+joshuakohut77@users.noreply.github.com>
2022-05-25 23:18:01 -0400

    Update trainer_battles.sql
    made id auto incremement

--- pokemon/sql/trainer_battles.sql
+++ pokemon/sql/trainer_battles.sql
@@ -4,7 +4,7 @@
 
 CREATE TABLE IF NOT EXISTS public.trainer_battles
 (
-    id bigint NOT NULL,
+    id bigint NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 9223372036854775807 CACHE 1 ),
     discord_id character varying(20) COLLATE pg_catalog."default" NOT NULL,
     "locationId" integer NOT NULL,
     enemy_uuid character varying(10) COLLATE pg_catalog."default" NOT NULL,
