6bb4fb3994a0b0b685755e742fd1069c61ed2bc9
Matt Sheehan <matt@mattsheehan.dev>
2022-05-28 22:53:00 -0400

    add store pagination

--- pokemon/pokemart.py
+++ pokemon/pokemart.py
@@ -3,6 +3,8 @@ from typing import Any, Dict, List, Union, TYPE_CHECKING
 
 
 import discord
+from discord_components import (ButtonStyle, Button, Interaction)
+from models.location import LocationModel
 
 if TYPE_CHECKING:
     from redbot.core.bot import Red
@@ -11,7 +13,7 @@ from redbot.core import commands
 
 import constant
 from services.trainerclass import trainer as TrainerClass
-from services.storeclass import store as StoreClass
+from services.storeclass import StoreItem, store as StoreClass
 
 from .abcd import MixinMeta
 
@@ -244,8 +246,30 @@ itemDisplayNames = {
     }
 }
 
+
+class StoreState:
+    storeList: List[List[StoreItem]]
+    idx: int
+    location: LocationModel
+
+    discordId: int
+    messageId: int
+    channelId: int
+
+    def __init__(self, discordId: int, messageId: int, channelId: int, location: LocationModel, storeList: List[List[StoreItem]], idx: int):
+        self.discordId = discordId
+        self.messageId = messageId
+        self.channelId = channelId
+
+        self.location = location
+        self.storeList = storeList
+        self.idx = idx
+
+
 class PokemartMixin(MixinMeta):
     """Pokemart"""
+
+    __store = {}
 
 
     @commands.group(name="pokemart", aliases=['mart'])
@@ -271,22 +295,103 @@ class PokemartMixin(MixinMeta):
 
         # Create the embed object
         # file = discord.File("data/cogs/CogManager/cogs/pokemon/sprites/items/poke-ball.png", filename="poke-ball.png")
-        embed = discord.Embed(title=f"Poké Mart - {location.name}")
+
+        state = StoreState(user.id, None, None, location, store.storeList, 0)
+
+        embed, btns = self.__storePageEmbed(user, state)
+
+
+        message = await ctx.send(
+            embed=embed,
+            components=btns
+        )
+        self.__store[str(user.id)] = StoreState(user.id, message.id, message.channel.id, store.storeList, 0)
+
+        await ctx.tick()
+
+
+    def __storePageEmbed(self, user: discord.Member, state: StoreState):
+        # Create the embed object
+        # file = discord.File("data/cogs/CogManager/cogs/pokemon/sprites/items/poke-ball.png", filename="poke-ball.png")
+        embed = discord.Embed(title=f"Poké Mart - {state.location.name}")
         embed.set_thumbnail(url=f"https://pokesprites.joshkohut.com/sprites/locations/poke_mart.png")
         # embed.set_author(name=f"{user.display_name}",
         #                  icon_url=str(user.avatar_url))
 
-        for item in store.storeList:
-            key = item['item']
+        firstList = state.storeList[state.idx]
+
+        for item in firstList:
+            key = item.name
+            price = item.price
+
             emoji = itemDisplayNames[key]['emoji']
             description = itemDisplayNames[key]['desc']
             name = itemDisplayNames[key]['name']
-            price = item['price']
 
             embed.add_field(name=f"{emoji}  {name} — {price}", value=description, inline=False)
 
-        await ctx.send(embed=embed)
-        await ctx.tick()
+
+        btns = []
+
+        if state.idx > 0:
+            btns.append(self.client.add_callback(
+                Button(style=ButtonStyle.gray, label='Previous', custom_id='previous'),
+                self.__on_prev_click
+            ))
+        if state.idx < len(state.storeList) - 1:
+            btns.append(self.client.add_callback(
+                Button(style=ButtonStyle.gray, label="Next", custom_id='next'),
+                self.__on_next_click
+            ))
+
+        return embed, [btns]
+
+
+    async def __on_next_click(self, interaction: Interaction):
+        user = interaction.user
+
+        if not self.__checkStoreState(user, interaction.message):
+            await interaction.send('This is not for you.')
+            return
+
+        state: StoreState = self.__store[str(user.id)]
+        state.idx = state.idx + 1
+
+        embed, btns = await self.__storePageEmbed(user, state)
+        message = await interaction.edit_origin(embed=embed, components=btns)
+        
+        state.messageId = message
+        self.__store[str(user.id)] = state     
+    
+
+    async def __on_prev_click(self, interaction: Interaction):
+        user = interaction.user
+
+        if not self.__checkStoreState(user, interaction.message):
+            await interaction.send('This is not for you.')
+            return
+
+        state: StoreState = self.__store[str(user.id)]
+        state.idx = state.idx - 1
+
+        embed, btns = await self.__storePageEmbed(user, state)
+        message = await interaction.edit_origin(embed=embed, components=btns)
+        
+        state.messageId = message
+        self.__store[str(user.id)] = state              
+
+
+    def __checkStoreState(self, user: discord.User, message: discord.Message):
+        state: StoreState
+        if str(user.id) not in self.__store.keys():
+            return False
+        else:
+            state = self.__store[str(user.id)]
+            if state.messageId != message.id:
+                return False
+        return True
+    
+
 
     @_pokemart.command()
     async def buy(self, ctx: commands.Context, item: str, count: int = 1) -> None:
