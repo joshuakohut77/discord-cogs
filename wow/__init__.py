fb0e7026ec12e102a0f01e20676947972b10bf3d
joshuakohut77 <joshuakohut77@gmail.com>
2022-07-16 20:35:13 -0400

    Update __init__.py

--- wow/__init__.py
+++ wow/__init__.py
@@ -4,7 +4,7 @@ from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     from redbot.core.bot import Red
 
-from .main import owenWilson
+from .main import owenWilson as wow
 
 def setup(bot: Red):
     bot.add_cog(wow(bot))
