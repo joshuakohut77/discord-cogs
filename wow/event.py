79dbdbe73b9c65ef83494eff8172ba77503671c1
joshuakohut77 <64916852+joshuakohut77@users.noreply.github.com>
2022-08-11 00:15:47 -0400

    Update event.py

--- wow/event.py
+++ wow/event.py
@@ -32,6 +32,6 @@ class EventMixin(MixinMeta):
                 shutil.copyfileobj(response.raw, out_file)
             file = discord.File('/tempfiles/wowclip.mp4')
             embed.set_image(url="attachment://%s" %videoLink)
-            await ctx.send(embed=embed, file=file)
+            await message.send(embed=embed, file=file)
 
 		
