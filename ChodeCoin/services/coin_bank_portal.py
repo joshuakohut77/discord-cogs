6a04f99f353d2b61a44ae62ef5238244ae7bfc0f
amabelma <11965165+amabelma@users.noreply.github.com>
2022-12-26 00:14:03 -0500

    Fix coin_bank.json to have a usable format. Create date_helper for creating timestamp strings. Implement json updating, adding, and reading. Update user_manager to use the new service for data reading.

--- ChodeCoin/services/coin_bank_portal.py
+++ ChodeCoin/services/coin_bank_portal.py
@@ -1,14 +1,36 @@
+import datetime
 import json
 import discord
 import json
 from discord import embeds
+from ..Helpers.date_helper import DateHelper
 
 class CoinBankPortal:
+    def __init__(self, date_helper=DateHelper()):
+        self.date_helper = date_helper
     def change_coin_count(self, target_user, amount):
-        print(f"adding {amount} to {target_user}'s ChodeCoin count.")
-        # Get User Value
-        # Add amount to User Value
-        # Set User's Value to new Value
+        with open("DB/coin_bank.json", "r+") as file:
+            bank_records = json.load(file)
+            for user in bank_records:
+                if user["name"] == target_user:
+                    current_coin_balance = user["coin_count"]
+                    new_coin_balance = current_coin_balance + amount
+                    user["coin_count"] = new_coin_balance
+                    break
+            json.dump(bank_records, file)
 
     def create_new_user(self, target_user):
-        print(f"creating user {target_user} in the database")
+        new_user = {"name": target_user, "coin_count": 0, "last_modified": self.date_helper.current_timestamp_string()}
+        with open("DB/coin_bank.json", "r+") as file:
+            file_data = json.load(file)
+            file_data["bank_records"].append(new_user)
+            file.seek(0)
+            json.dump(file_data, file, indent=4)
+
+    def user_exists(self, target_user):
+        with open("DB/coin_bank.json", "r") as file:
+            bank_records = json.load(file)
+            for user in bank_records:
+                if user["name"] == target_user:
+                    return True
+            return False
