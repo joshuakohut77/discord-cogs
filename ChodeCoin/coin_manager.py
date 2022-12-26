1c990fb9488080ceaf0d410b0ffc4b2c2f0af46e
Mark Amabeli <markamabeli@gmail.com>
2022-12-25 22:23:12 -0500

    Refactor classes with dependency injection. Organize file structure better.

--- ChodeCoin/coin_manager.py
+++ ChodeCoin/coin_manager.py
@@ -3,31 +3,28 @@ import discord
 import json
 from discord import embeds
 from user_manager import UserManager
+from services.coin_bank_portal import CoinBankPortal
 
 
 class CoinManager:
+    def __init__(self, user_manager=UserManager(), coin_bank_portal=CoinBankPortal()):
+        self.user_manager = user_manager
+        self.coin_bank_portal = coin_bank_portal
+
     def process_plus_plus(self, target_user):
-        user_manager = UserManager()
-        user_exists = user_manager.user_exists(target_user)
+        user_exists = self.user_manager.user_exists(target_user)
 
         if user_exists:
-            self.change_coin_count(target_user, 1)
+            self.coin_bank_portal.change_coin_count(target_user, 1)
         else:
-            user_manager.create_user(target_user)
-            self.change_coin_count(target_user, 1)
+            self.user_manager.create_user(target_user)
+            self.coin_bank_portal.change_coin_count(target_user, 1)
 
     def process_minus_minus(self, target_user):
-        user_manager = UserManager()
-        user_exists = user_manager.user_exists(target_user)
+        user_exists = self.user_manager.user_exists(target_user)
 
         if user_exists:
-            self.change_coin_count(target_user, -1)
+            self.coin_bank_portal.change_coin_count(target_user, -1)
         else:
-            user_manager.create_user(target_user)
-            self.change_coin_count(target_user, -1)
-
-    def change_coin_count(self, target_user, amount):
-        print(f"adding {amount} to {target_user}'s ChodeCoin count.")
-        # Get User Value
-        # Add amount to User Value
-        # Set User's Value to new Value
+            self.user_manager.create_user(target_user)
+            self.coin_bank_portal.change_coin_count(target_user, -1)
--- ChodeCoin/user_manager.py
+++ ChodeCoin/user_manager.py
@@ -2,16 +2,19 @@ import json
 import discord
 import json
 from discord import embeds
+from services.coin_bank_portal import CoinBankPortal
 
 
 class UserManager:
+    def __init__(self, coin_bank_portal=CoinBankPortal()):
+        self.coin_bank_portal = coin_bank_portal
     def user_exists(self, target_user: str):
-        with open('coin_bank.json', 'r') as openfile:
+        with open('DB/coin_bank.json', 'r') as openfile:
             coin_bank = json.load(openfile)
             for user in coin_bank:
                 if user['name'] == target_user:
                     return True
             return False
 
-    def create_user(self, target_user):
-        print("Creating user")
+    def create_new_user(self, target_user):
+        self.coin_bank_portal.create_new_user(target_user)
