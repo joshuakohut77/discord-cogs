b56cd8fd97e2f3d3e1dfbd4bda5709f738ccf21d
amabelma <11965165+amabelma@users.noreply.github.com>
2022-12-26 22:10:18 -0500

    Move files into organized directories. No idea if the non init files need to be out of the core directory but yolo

--- ChodeCoin/services/coin_bank_portal.py
+++ ChodeCoin/services/coin_bank_portal.py
@@ -3,13 +3,13 @@ import json
 import discord
 import json
 from discord import embeds
-from ..Helpers.date_helper import DateHelper
+from ..helpers.date_helper import DateHelper
 
 class CoinBankPortal:
     def __init__(self, date_helper=DateHelper()):
         self.date_helper = date_helper
     def change_coin_count(self, target_user, amount):
-        with open("DB/coin_bank.json", "r+") as file:
+        with open("db/coin_bank.json", "r+") as file:
             bank_records = json.load(file)
             for user in bank_records:
                 if user["name"] == target_user:
@@ -21,14 +21,14 @@ class CoinBankPortal:
 
     def create_new_user(self, target_user):
         new_user = {"name": target_user, "coin_count": 0, "last_modified": self.date_helper.current_timestamp_string()}
-        with open("DB/coin_bank.json", "r+") as file:
+        with open("db/coin_bank.json", "r+") as file:
             file_data = json.load(file)
             file_data["bank_records"].append(new_user)
             file.seek(0)
             json.dump(file_data, file, indent=4)
 
     def user_exists(self, target_user):
-        with open("DB/coin_bank.json", "r") as file:
+        with open("db/coin_bank.json", "r") as file:
             bank_records = json.load(file)
             for user in bank_records:
                 if user["name"] == target_user:
