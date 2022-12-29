2bbdec72c90eb8020bbe9de92776e4963fc66ab1
amabelma <11965165+amabelma@users.noreply.github.com>
2022-12-29 13:23:18 -0500

    refactoring of organization for project. WIP extract backend from frontend. Implement Emoji detection for plusplus and minusminus.

--- ChodeCoin/__init__.py
+++ ChodeCoin/__init__.py
@@ -4,7 +4,8 @@ from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     from redbot.core.bot import Red
 
-from ChodeCoin.core.main import ChodeCoin as chode_coin
+from ChodeCoin.core.main import ChodeCoin
+
 
 def setup(bot: Red):
-    bot.add_cog(chode_coin(bot))
+    bot.add_cog(ChodeCoin(bot))
---
+++ ChodeCoin/_test/utilities/test_coin_manager.py
@@ -0,0 +1,20 @@
+import unittest
+from mock import Mock
+from ChodeCoin.utilities.coin_manager import CoinManager
+from ChodeCoin.utilities.user_manager import UserManager
+from ChodeCoin.services.coin_bank_portal import CoinBankPortal
+
+
+class TestCoinManager(unittest.TestCase):
+    def Given_process_plus_plus_When_targetuser_exists_Then_calls_change_coin_count_with_value_of_one(self) -> None:
+        target_user = "UserThatExists"
+        mock_usermanager = Mock(UserManager)
+        mock_usermanager.user_exists = True
+        mock_coinbankportal = Mock(CoinBankPortal)
+        coin_manager = CoinManager(mock_usermanager, mock_coinbankportal)
+
+        coin_manager.process_plus_plus(target_user)
+
+        self.assertTrue(mock_coinbankportal.mockSetExpecation("change_coin_count", mock_coinbankportal.change_coin_count, 0, 2))
+        self.assertTrue(mock_usermanager.mockSetExpectation("create_new_user", mock_usermanager.create_new_user, 0, 0))
+
