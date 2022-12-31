f34612d1383d3aea66fd96682d7fe46089b94461
Mark Amabeli <mark.amabeli@rhsps.com>
2022-12-30 22:38:19 -0500

    Designate regex strings as regex strings. Convert re.search calls into strings for processing. Improve falsy detection marginally (still sucks)

--- ChodeCoin/utilities/message_manager.py
+++ ChodeCoin/utilities/message_manager.py
@@ -34,8 +34,6 @@ class MessageManager:
 
         return False, ""
 
-
-
     def generate_reply(self, targeted_user, process):
         if process == "plus_plus":
             return f"Gave {targeted_user} a ChodeCoin!"
@@ -44,50 +42,50 @@ class MessageManager:
 
     def extract_targeted_user(self, targeted_user, process):
         if process == "Text":
-            preformat = re.search("@.{2,32}?[+-]", targeted_user)
-            formatted_user = preformat[:len(str(targeted_user))-1].strip()
+            preformat = re.search(r"@.{2,32}?[+-]", targeted_user)
+            formatted_user = preformat.group(0)[:len(str(targeted_user)) - 1].strip()
         elif process == "Eggplant":
-            preformat = re.search("@.{2,32}?:", targeted_user)
-            formatted_user = preformat[:len(str(targeted_user))-1].strip()
+            preformat = re.search(r"@.{2,32}?:", targeted_user)
+            formatted_user = preformat.group(0)[:len(str(targeted_user)) - 1].strip()
         elif process == "No":
-            preformat = re.search("@.{2,32}?:", targeted_user)
-            formatted_user = preformat[:len(str(targeted_user))-1].strip()
+            preformat = re.search(r"@.{2,32}?:", targeted_user)
+            formatted_user = preformat.group(0)[:len(str(targeted_user)) - 1].strip()
         else:
             raise NameError("Process not defined")
 
         return formatted_user
 
     def find_plus_plus(self, message):
-        targeted_user = ""
-        targeted_user = re.search("@.{2,32}?[+]{2}", message)
-        if targeted_user != "":
+        search_result = re.search(r"@.{2,32}?[+]{2}", message)
+        if search_result:
+            targeted_user = search_result.group(0)
             formatted_user = self.extract_targeted_user(targeted_user, "Text")
             return True, formatted_user
         else:
             return False, message
 
     def find_minus_minus(self, message):
-        targeted_user = ""
-        targeted_user = re.search("@.{2,32}?-{2}", message)
-        if targeted_user != "":
+        search_result = re.search(r"@.{2,32}?-{2}", message)
+        if search_result:
+            targeted_user = search_result.group(0)
             formatted_user = self.extract_targeted_user(targeted_user, "Text")
             return True, formatted_user
         else:
             return False, message
 
     def find_eggplant_eggplant(self, message):
-        targeted_user = ""
-        targeted_user = re.search("@.{2,32}?((:eggplant:)\s*){2}", message)
-        if targeted_user != "":
+        search_result = re.search(r"@.{2,32}?((:eggplant:)\s*){2}", message)
+        if search_result:
+            targeted_user = search_result.group(0)
             formatted_user = self.extract_targeted_user(targeted_user, "Text")
             return True, formatted_user
         else:
             return False, message
 
     def find_no_no(self, message):
-        targeted_user = ""
-        targeted_user = re.search("@.{2,32}?((:No:)\s*){2}", message)
-        if targeted_user != "":
+        search_result = re.search(r"@.{2,32}?((:No:)\s*){2}", message)
+        if search_result:
+            targeted_user = search_result.group(0)
             formatted_user = self.extract_targeted_user(targeted_user, "Text")
             return True, formatted_user
         else:
