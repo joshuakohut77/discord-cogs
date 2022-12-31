34518d78b6438969cd644f298548b3c821f27039
Mark Amabeli <mark.amabeli@rhsps.com>
2022-12-30 23:04:03 -0500

    Use pathlib to try to point at the database json file

--- ChodeCoin/services/coin_bank_portal.py
+++ ChodeCoin/services/coin_bank_portal.py
@@ -1,12 +1,14 @@
 import json
+from pathlib import Path
 from ..helpers.date_helper import DateHelper
 
 
 class CoinBankPortal:
     def __init__(self, date_helper=DateHelper()):
         self.date_helper = date_helper
+        self.db_path = f"{Path(__file__).parents[1]}/db/coin_bank.json"
     def change_coin_count(self, target_user, amount):
-        with open("ChodeCoin/db/coin_bank.json", "r+") as file:
+        with open(self.db_path, "r+") as file:
             bank_records = json.load(file)
             for user in bank_records:
                 if user["name"] == target_user:
@@ -18,14 +20,14 @@ class CoinBankPortal:
 
     def create_new_user(self, target_user):
         new_user = {"name": target_user, "coin_count": 0, "last_modified": self.date_helper.current_timestamp_string()}
-        with open("ChodeCoin/db/coin_bank.json", "r+") as file:
+        with open(self.db_path, "r+") as file:
             file_data = json.load(file)
             file_data["bank_records"].append(new_user)
             file.seek(0)
             json.dump(file_data, file, indent=4)
 
     def user_exists(self, target_user):
-        with open("ChodeCoin/db/coin_bank.json", "r") as file:
+        with open(self.db_path, "r") as file:
             bank_records = json.load(file)
             for user in bank_records:
                 if user["name"] == target_user:
