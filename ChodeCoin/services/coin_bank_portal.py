e504374e53bcf0e591691389421df1940556b4f4
Mark Amabeli <mark.amabeli@rhsps.com>
2022-12-30 22:53:11 -0500

    Attempt to fix string path for accessing json file

--- ChodeCoin/services/coin_bank_portal.py
+++ ChodeCoin/services/coin_bank_portal.py
@@ -6,7 +6,7 @@ class CoinBankPortal:
     def __init__(self, date_helper=DateHelper()):
         self.date_helper = date_helper
     def change_coin_count(self, target_user, amount):
-        with open("db/coin_bank.json", "r+") as file:
+        with open("ChodeCoin/db/coin_bank.json", "r+") as file:
             bank_records = json.load(file)
             for user in bank_records:
                 if user["name"] == target_user:
@@ -18,14 +18,14 @@ class CoinBankPortal:
 
     def create_new_user(self, target_user):
         new_user = {"name": target_user, "coin_count": 0, "last_modified": self.date_helper.current_timestamp_string()}
-        with open("db/coin_bank.json", "r+") as file:
+        with open("ChodeCoin/db/coin_bank.json", "r+") as file:
             file_data = json.load(file)
             file_data["bank_records"].append(new_user)
             file.seek(0)
             json.dump(file_data, file, indent=4)
 
     def user_exists(self, target_user):
-        with open("db/coin_bank.json", "r") as file:
+        with open("ChodeCoin/db/coin_bank.json", "r") as file:
             bank_records = json.load(file)
             for user in bank_records:
                 if user["name"] == target_user:
