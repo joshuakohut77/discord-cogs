b200bc81283ce57302d6edf890531d66de9cab14
Mark Amabeli <mark.amabeli@rhsps.com>
2022-12-31 00:24:58 -0500

    Overwrite file for change_coin_count

--- ChodeCoin/services/coin_bank_portal.py
+++ ChodeCoin/services/coin_bank_portal.py
@@ -7,6 +7,7 @@ class CoinBankPortal:
     def __init__(self, date_helper=DateHelper()):
         self.date_helper = date_helper
         self.db_path = f"{Path(__file__).parents[1]}/db/coin_bank.json"
+
     def change_coin_count(self, target_user, amount):
         with open(self.db_path, "r+") as file:
             bank = json.load(file)
@@ -16,14 +17,15 @@ class CoinBankPortal:
                     new_coin_balance = current_coin_balance + amount
                     bank_record["coin_count"] = new_coin_balance
                     break
-            #json.dump(bank, file)
+        with open(self.db_path, "wt") as file:
+            json.dump(bank, file)
 
     def create_new_user(self, target_user):
         new_user = {"name": target_user, "coin_count": 0, "last_modified": self.date_helper.current_timestamp_string()}
         with open(self.db_path, "r+") as file:
             bank = json.load(file)
             bank["bank_records"].append(new_user)
-            #json.dump(bank, file, indent=4)
+            json.dump(bank, file, indent=4)
 
     def user_exists(self, target_user):
         with open(self.db_path, "r") as file:
