0f1ea540fdfcc91fe900bf06c679d9401e194a24
Mark Amabeli <mark.amabeli@rhsps.com>
2023-01-02 12:25:32 -0500

    No working change, add a comment for why the guard method isn't working. Don't care to fix it for now

--- ChodeCoin/utilities/guard.py
+++ ChodeCoin/utilities/guard.py
@@ -5,6 +5,7 @@ class Guard:
     def __init__(self, message_reader=MessageReader()):
         self.message_reader = message_reader
 
+    # Doesn't work at the moment due to the way message.author differs from the ping'd user's ID
     def against_self_plusplus(self, message, message_author):
         targeted_user = self.message_reader.extract_targeted_user(message, "Text")
         if message_author == targeted_user:
