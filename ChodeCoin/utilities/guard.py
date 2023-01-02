bdfe5ac4a6b1f49856c46b6848d0646179c03827
Mark Amabeli <mark.amabeli@rhsps.com>
2023-01-02 11:44:05 -0500

    Fix message vs message content send to validator

--- ChodeCoin/utilities/guard.py
+++ ChodeCoin/utilities/guard.py
@@ -6,7 +6,8 @@ class Guard:
         self.message_reader = message_reader
 
     def against_self_plusplus(self, message_author, message):
-        targeted_user = self.message_reader.extract_targeted_user(message, "Text")
+        msg: str = message.content
+        targeted_user = self.message_reader.extract_targeted_user(str, "Text")
         if message_author == targeted_user:
             return "You silly goose you can't updoot yourself o_O"
         else:
