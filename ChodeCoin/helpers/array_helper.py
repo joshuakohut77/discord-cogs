6da9e572c8da4a805f07eb46d8a154efcec91ca8
Mark Amabeli <mark.amabeli@rhsps.com>
2023-01-03 00:12:35 -0500

    Hopefully fix formatting that my sick brain at midnight apparently can't comprehend

--- ChodeCoin/helpers/array_helper.py
+++ ChodeCoin/helpers/array_helper.py
@@ -23,8 +23,8 @@ class ArrayHelper:
     def add_if_in_wealthiest_group(self, user_array, user_to_validate: User, return_count):
         return self.insert_into_array_if_wealthy_enough(user_array, user_to_validate, return_count)
 
-    def insert_into_array_if_wealthy_enough(self, user_array: [], user_to_add: User, return_count):
-        user_array.insert(user_to_add)
+    def insert_into_array_if_wealthy_enough(self, user_array: [User], user_to_add: User, return_count):
+        user_array.insert(0, user_to_add)
         if len(user_array) > return_count:
             user_array = self.sort_array(user_array)
             user_array = user_array[:-1]
