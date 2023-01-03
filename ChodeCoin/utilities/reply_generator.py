f73de5c378b5111862be02022ccec223f4b4d535
Mark Amabeli <mark.amabeli@rhsps.com>
2023-01-02 23:26:54 -0500

    Another test

--- ChodeCoin/utilities/reply_generator.py
+++ ChodeCoin/utilities/reply_generator.py
@@ -7,7 +7,7 @@ def generate_leaderboard_reply(user_array: []):
     embed = discord.Embed()
     embed = discord.Embed(title="ChodeCoin Leaderboard", color=0x0b1bf4)
     embed.add_field(name="Name", value="", inline=True)
-    embed.add_field(name="ChodeCoin", value=str(fml), inline=True)
+    embed.add_field(name="ChodeCoin", value="" + "test", inline=True)
     return "", embed
 
 
