5f408697a6969d76bcf942fc079fd0e7e81f81f4
Mark Amabeli <mark.amabeli@rhsps.com>
2023-01-05 16:41:33 -0500

    Update guard.py to use enums for format_targeted_user

--- ChodeCoin/utilities/guard.py
+++ ChodeCoin/utilities/guard.py
@@ -1,3 +1,4 @@
+from ChodeCoin.enums.command_type import CommandType
 from ChodeCoin.utilities.message_reader import MessageReader
 
 
@@ -7,7 +8,7 @@ class Guard:
 
     # Doesn't work at the moment due to the way message.author differs from the ping'd user's ID
     def against_self_plusplus(self, message, message_author):
-        targeted_user = self.message_reader.format_targeted_user(message, "Text")
+        targeted_user = self.message_reader.format_targeted_user(message, CommandType.text)
         if message_author == targeted_user:
             return "You silly goose you can't updoot yourself o_O"
         else:
