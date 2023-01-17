15c9c3c42cadb34e44d975e2b559e7eec34ac2c7
Mark Amabeli <mark.amabeli@rhsps.com>
2023-01-16 21:23:10 -0500

    insert debug line

--- ChodeCoin/Backend/workflows/main_work_flow.py
+++ ChodeCoin/Backend/workflows/main_work_flow.py
@@ -37,7 +37,8 @@ class WorkFlow:
             return self.targeted_coin_count_workflow.process_targeted_coin_count_request(message, author)
 
         elif process == RequestFor.dank_hof:
-            return self.dank_hof_workflow.process_dank_hof_request(message, author)
+            # return self.dank_hof_workflow.process_dank_hof_request(message, author)
+            return "Josh Sucks", None
 
         else:
             return None, None
