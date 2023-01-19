018705d61e0f88de33c50aac61f1cde1358c7285
Mark Amabeli <mark.amabeli@rhsps.com>
2023-01-19 13:31:34 -0500

    Fix convert_to_discord_user to not double convert a pre-converted discord user

--- ChodeCoin/Backend/_BackendTests/utilities/test_message_reader.py
+++ ChodeCoin/Backend/_BackendTests/utilities/test_message_reader.py
@@ -205,7 +205,7 @@ class TestMessageReader:
         # Assert
         assert expected == actual
 
-    @pytest.mark.parametrize("message, formatted_user", [("!setpermission 500047678378344449 admin", "<@500047678378344449>"), ("!setpermission 1019075447532826726 admin", "<@1019075447532826726>"), ("!setpermission 1019075447532826726 admin", "<@1019075447532826726>")])
+    @pytest.mark.parametrize("message, formatted_user", [("!setpermission 500047678378344449 admin", "<@500047678378344449>"), ("!setpermission 1019075447532826726 admin", "<@1019075447532826726>"), ("!setpermission 10190754475328267269 admin", "<@10190754475328267269>")])
     def test_GIVEN_find_targeted_admin_data_WHEN_valid_admin_request_is_provided_THEN_returns_formatted_user(self, message, formatted_user) -> None:
         # Arrange Act
         target_user, new_admin_level = find_targeted_admin_data(message)
