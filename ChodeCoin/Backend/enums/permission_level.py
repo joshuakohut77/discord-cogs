fd4d9d58ba074f638ab03dcd15ece1c6b735451c
Mark Amabeli <mark.amabeli@rhsps.com>
2023-01-19 14:07:36 -0500

    Nevermind it didn't compile

--- ChodeCoin/Backend/enums/permission_level.py
+++ ChodeCoin/Backend/enums/permission_level.py
@@ -6,15 +6,3 @@ class PermissionLevel(Enum):
     admin = 2
     viewer = 3
     none = 4
-
-    def from_str(label):
-        if label in ('owner', 'Owner'):
-            return PermissionLevel.owner
-        elif label in ('admin', 'Admin'):
-            return PermissionLevel.admin
-        elif label in ('viewer', 'Viewer'):
-            return PermissionLevel.viewer
-        elif label in ('none', 'None'):
-            return PermissionLevel.none
-        else:
-            raise NotImplementedError
