f4e436a8d24404f8f12785f3e9d69e05b306b597
Mark Amabeli <mark.amabeli@rhsps.com>
2023-01-22 12:48:34 -0500

    Add discord user check to convert_to_discord_user. The name and purpose of the command isn't perfect though

--- ChodeCoin/Backend/helpers/string_helper.py
+++ ChodeCoin/Backend/helpers/string_helper.py
@@ -1,8 +1,13 @@
-def convert_to_discord_user(discord_user):
-    if discord_user.__str__()[:2] != "<@":
-        return f"<@{discord_user}>"
+import re
+
+
+def convert_to_discord_user(target_user):
+    user_name_check = re.search(r"\d{18,20}", target_user)
+    if user_name_check:
+        if target_user.__str__()[:2] != "<@":
+            return f"<@{target_user}>"
     else:
-        return discord_user
+        return target_user
 
 
 class StringHelper:
