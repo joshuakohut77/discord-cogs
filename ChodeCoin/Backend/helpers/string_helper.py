629516151c67c895ebdc3cfc92c18dffeb7ac873
Mark Amabeli <mark.amabeli@rhsps.com>
2023-01-22 12:54:45 -0500

    fix typing bug

--- ChodeCoin/Backend/helpers/string_helper.py
+++ ChodeCoin/Backend/helpers/string_helper.py
@@ -2,10 +2,12 @@ import re
 
 
 def convert_to_discord_user(target_user):
-    user_name_check = re.search(r"\d{18,20}", target_user)
+    user_name_check = re.search(r"\d{18,20}", target_user.__str__())
     if user_name_check:
         if target_user.__str__()[:2] != "<@":
             return f"<@{target_user}>"
+        else:
+            return target_user
     else:
         return target_user
 
