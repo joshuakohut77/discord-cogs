077fc4c560e8e82990ae9aacb9d9898c42629e65
Mark Amabeli <mark.amabeli@rhsps.com>
2023-01-22 14:11:33 -0500

    Fix bug in timestamp string generator

--- ChodeCoin/Backend/helpers/timestamp_helper.py
+++ ChodeCoin/Backend/helpers/timestamp_helper.py
@@ -4,5 +4,5 @@ import datetime
 class TimestampHelper:
     def current_timestamp_string(self):
         timestamp = datetime.datetime.now()
-        timestamp_string = timestamp.strftime("%Y-%m-% , %H:%M:%S")
+        timestamp_string = timestamp.strftime("%Y-%m-%d %H:%M:%S")
         return timestamp_string
