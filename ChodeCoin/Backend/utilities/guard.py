baebbaa00fea3d84dd0ae6e893f8098ed4f2b108
Mark Amabeli <mark.amabeli@rhsps.com>
2023-01-27 20:16:01 -0500

    Insert debug line

--- ChodeCoin/Backend/utilities/guard.py
+++ ChodeCoin/Backend/utilities/guard.py
@@ -10,8 +10,8 @@ class Guard:
     # Doesn't work at the moment due to the way message.author differs from the ping'd user's ID
     def against_self_plusplus(self, message, message_author):
         text_targeted_user = convert_to_discord_user(self.message_reader.format_targeted_user(message, CommandType.text))
-        emoji_targeted_user = convert_to_discord_user(self.message_reader.format_targeted_user(message, CommandType.text))
+        emoji_targeted_user = convert_to_discord_user(self.message_reader.format_targeted_user(message, CommandType.emoji_plus_plus))
         if message_author == text_targeted_user or message_author == emoji_targeted_user:
             return "You silly goose you can't updoot yourself o_O"
         else:
-            return None
+            return text_targeted_user
