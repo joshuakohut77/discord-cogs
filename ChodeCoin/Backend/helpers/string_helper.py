a8c49001af12837383225e75ad31c5c8d254ea81
Mark Amabeli <mark.amabeli@rhsps.com>
2023-01-27 21:50:38 -0500

    This shouldn't work but idk anything anymore

--- ChodeCoin/Backend/helpers/string_helper.py
+++ ChodeCoin/Backend/helpers/string_helper.py
@@ -5,7 +5,7 @@ def convert_to_discord_user(target_user):
     user_name_check = re.search(r"\d{18,20}", target_user.__str__())
     if user_name_check:
         if target_user.__str__()[:2] != "<@":
-            return f"<@{target_user}>"
+            return f"<@{target_user}"
         else:
             return target_user
     else:
