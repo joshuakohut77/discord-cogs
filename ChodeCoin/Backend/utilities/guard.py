277f1915ee3ef412d452aaadcf5894876d81062b
Mark Amabeli <mark.amabeli@rhsps.com>
2023-01-27 19:04:15 -0500

    Kind of start to add guard unit tests but also not because I don't remember the format for the inputted username and I want to do the help page stuff first

--- ChodeCoin/Backend/utilities/guard.py
+++ ChodeCoin/Backend/utilities/guard.py
@@ -1,4 +1,5 @@
 from ChodeCoin.Backend.enums.command_type import CommandType
+from ChodeCoin.Backend.helpers.string_helper import convert_to_discord_user
 from ChodeCoin.Backend.utilities.message_reader import MessageReader
 
 
@@ -8,8 +9,9 @@ class Guard:
 
     # Doesn't work at the moment due to the way message.author differs from the ping'd user's ID
     def against_self_plusplus(self, message, message_author):
-        targeted_user = self.message_reader.format_targeted_user(message, CommandType.text)
-        if message_author == targeted_user:
+        text_targeted_user = convert_to_discord_user(self.message_reader.format_targeted_user(message, CommandType.text))
+        emoji_targeted_user = convert_to_discord_user(self.message_reader.format_targeted_user(message, CommandType.text))
+        if message_author == text_targeted_user or message_author == emoji_targeted_user:
             return "You silly goose you can't updoot yourself o_O"
         else:
             return None
