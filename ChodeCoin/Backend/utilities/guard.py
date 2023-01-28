36539900a9046ca21112648e885eaf3f1a2ce85d
Mark Amabeli <mark.amabeli@rhsps.com>
2023-01-27 21:57:00 -0500

    Fix it but will obviously need to fix later because HECK discord username inputs lol

--- ChodeCoin/Backend/utilities/guard.py
+++ ChodeCoin/Backend/utilities/guard.py
@@ -8,8 +8,8 @@ class Guard:
 
     # Doesn't work at the moment due to the way message.author differs from the ping'd user's ID
     def against_self_plus_plus(self, target_user, message_author):
-        formatted_author = convert_to_discord_user(message_author)
+        formatted_author = f"{convert_to_discord_user(message_author)}>"
         if formatted_author == target_user:
             return "You silly goose you can't updoot yourself o_O"
         else:
-            return f"{formatted_author}, {target_user}"
+            return None
