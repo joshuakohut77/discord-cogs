b17a7d02ec81dda42f9d276387886599574ff578
Mark Amabeli <mark.amabeli@rhsps.com>
2023-01-27 21:54:10 -0500

    Fix guard issue

--- ChodeCoin/Backend/utilities/guard.py
+++ ChodeCoin/Backend/utilities/guard.py
@@ -1,3 +1,4 @@
+from ChodeCoin.Backend.helpers.string_helper import convert_to_discord_user
 from ChodeCoin.Backend.utilities.message_reader import MessageReader
 
 
@@ -7,7 +8,7 @@ class Guard:
 
     # Doesn't work at the moment due to the way message.author differs from the ping'd user's ID
     def against_self_plus_plus(self, target_user, message_author):
-        if message_author == target_user:
+        if convert_to_discord_user(message_author) == target_user:
             return "You silly goose you can't updoot yourself o_O"
         else:
-            return f"{target_user}, {message_author}"
+            return None
