aa5caca887af138b3c2aae7a5e6334fe1900989c
Mark Amabeli <mark.amabeli@rhsps.com>
2023-01-27 21:55:54 -0500

    Well shoot another debug try

--- ChodeCoin/Backend/utilities/guard.py
+++ ChodeCoin/Backend/utilities/guard.py
@@ -8,7 +8,8 @@ class Guard:
 
     # Doesn't work at the moment due to the way message.author differs from the ping'd user's ID
     def against_self_plus_plus(self, target_user, message_author):
-        if convert_to_discord_user(message_author) == target_user:
+        formatted_author = convert_to_discord_user(message_author)
+        if formatted_author == target_user:
             return "You silly goose you can't updoot yourself o_O"
         else:
-            return None
+            return f"{formatted_author}, {target_user}"
