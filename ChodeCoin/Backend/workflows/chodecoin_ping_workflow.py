55820617e8a9948d2212add90673facf3f2e699a
Mark Amabeli <mark.amabeli@rhsps.com>
2023-02-02 19:33:26 -0500

    fix typo

--- ChodeCoin/Backend/workflows/chodecoin_ping_workflow.py
+++ ChodeCoin/Backend/workflows/chodecoin_ping_workflow.py
@@ -33,7 +33,7 @@ class ChodeCoinPingWorkflow:
             if return_message is None:
                 self.coin_manager.process_plus_plus(targeted_user)
                 # return_message = self.reply_generator.generate_chodecoin_ping_reply(targeted_user, 1)
-                return_message = f"{message}, {author[1:len(str(author))]}"
+                return_message = f"{message}, {str(author)[1:len(str(author))]}"
             return return_message
 
         if self.message_reader.is_emoji_plus_plus_command(message):
