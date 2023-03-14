87b596663488024210e58b284f04e63bfa8e3c64
mark-amabeli <markamabeli@gmail.com>
2023-03-14 19:47:36 -0400

    Call correct data extraction method for process_set_info_request

--- ChodeCoin/Backend/workflows/set_info_workflow.py
+++ ChodeCoin/Backend/workflows/set_info_workflow.py
@@ -1,4 +1,4 @@
-from ChodeCoin.Backend.utilities.message_reader import MessageReader, is_set_info_command, find_targeted_permission_data
+from ChodeCoin.Backend.utilities.message_reader import MessageReader, is_set_info_command, find_targeted_permission_data, find_set_info_data
 from ChodeCoin.Backend.utilities.reply_generator import generate_coin_count_updated_reply, generate_permission_no_permission_reply, generate_command_error_reply
 from ChodeCoin.Backend.utilities.info_manager import InfoManager
 from ChodeCoin.Backend.utilities.user_manager import UserManager
@@ -26,8 +26,7 @@ class SetInfoWorkflow:
         self.message_reader = message_reader
 
     def process_set_info_request(self, message, author):
-        formatted_message = message.lower()
-        target_user, new_value = find_targeted_permission_data(formatted_message)
+        target_user, new_value = find_set_info_data(message)
         if target_user is not None and new_value is not None:
             if self.user_manager.is_admin_user(convert_to_discord_user(author)):
                 self.user_manager.set_coin_count(target_user, new_value)
