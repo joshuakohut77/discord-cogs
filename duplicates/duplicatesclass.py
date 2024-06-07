a1d90a161ccee1f83187604eefaa25a90fb326b7
joshuakohut77 <joshuakohut77@gmail.com>
2024-06-07 01:59:20 -0400

    Update duplicatesclass.py

--- duplicates/duplicatesclass.py
+++ duplicates/duplicatesclass.py
@@ -28,7 +28,7 @@ class Duplicates():
     def insert_message(msgHash, username):
         
         db = dbconn()
-        queryString = 'INSERT INTO public."duplicateMessage"("MessageHash", "Username") VALUES (' + str('sdklfs') + ', ' + str(username) + ')'
+        queryString = 'INSERT INTO public."duplicateMessage"("MessageHash", "Username") VALUES ("sdklfs", ' + str(username) + ')'
         values = (msgHash, username)
         db.execute(queryString)
         return
