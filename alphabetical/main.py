d4fa0bab09f064a7ac1981bf2c7ea1a45b44244f
joshuakohut77 <joshuakohut77@gmail.com>
2024-06-06 23:41:30 -0400

    Created Alphabetical Soup cog

---
+++ alphabetical/main.py
@@ -0,0 +1,22 @@
+from __future__ import annotations
+from typing import Any, Dict, List, TYPE_CHECKING
+from abc import ABCMeta
+
+if TYPE_CHECKING:
+    from redbot.core.bot import Red
+
+import discord
+from redbot.core import Config, commands
+
+from .event import EventMixin
+
+class CompositeClass(commands.CogMeta, ABCMeta):
+    __slots__: tuple = ()
+    pass
+
+class Alphabetical(EventMixin, commands.Cog, metaclass=CompositeClass):
+    """alphabetical"""
+	
+    def __init__(self, bot: Red):
+        self.bot: Red = bot
+
