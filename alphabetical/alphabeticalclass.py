f9284854acbee35c9ab8696fc39d545b570c4fe3
joshuakohut77 <joshuakohut77@gmail.com>
2024-06-06 23:48:44 -0400

    Update alphabeticalclass.py
    removed unused function and added a check to limit words to 5+ in a sentence

--- alphabetical/alphabeticalclass.py
+++ alphabetical/alphabeticalclass.py
@@ -10,30 +10,9 @@ class Alphabetical():
     def are_words_alphabetical_order(sentence):
         # Split the sentence into words
         words = sentence.split()
+        if len(words) < 5:
+            return False
         # Get the first letter of each word, converted to lowercase for case insensitivity
         first_letters = [word[0].lower() for word in words]
         # Compare the list of first letters with its sorted version
         return first_letters == sorted(first_letters)
-
-    def postAlphabeticalMessage(self):
-        url = "https://owen-wilson-wow-api.onrender.com/wows/random"
-        headers = {"Accept": "application/json"}
-        response = requests.get(url, headers=headers)
-        jsonResponse = json.loads(response.text)[0]
-        highestQuality = list(jsonResponse['video'].keys())[0]
-        videoLink = jsonResponse['video'][highestQuality]
-        
-        r = requests.get(videoLink)
-        with open("/tempfiles/wowclip.mp4", 'wb') as f:
-            f.write(r.content) 
-        file = discord.File('/tempfiles/wowclip.mp4')
-
-        embed = discord.Embed()
-        embed=discord.Embed(title="Owen Wilson", url="https://www.tomorrowtides.com/owen-wilson-movies.html", color=0x0b1bf4)
-        embed.set_thumbnail(url=jsonResponse["poster"])
-        embed.add_field(name="Movie", value=jsonResponse["movie"], inline=True)
-        embed.add_field(name="Year", value=str(jsonResponse['year']), inline=True)
-        embed.add_field(name="Character", value=jsonResponse['character'], inline=True)
-        embed.add_field(name="Wow in Movie", value=str(jsonResponse['current_wow_in_movie']) + "/" + str(jsonResponse['total_wows_in_movie']), inline=True)
-        embed.add_field(name="Full Line", value=jsonResponse['full_line'], inline=False)
-        return embed, file
