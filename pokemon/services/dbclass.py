9e5edc968c263844b069429785eb111b4eb74e62
joshuakohut77 <joshuakohut77@gmail.com>
2024-12-27 23:06:36 -0500

    Update dbclass.py

--- pokemon/services/dbclass.py
+++ pokemon/services/dbclass.py
@@ -21,23 +21,23 @@ class db:
     def __init__(self, params=None):
         self.faulted = False
         # TODO: need a better way to pass in db configs through all the objects.
-        self.conn = pg.connect(
-            host=(
-                params and params.host) or "db-redbot-nyc3-42069-do-user-1692759-0.b.db.ondigitalocean.com",
-            dbname=(params and params.dbname) or "pokemon_db",
-            user=(params and params.user) or "redbot",
-            # todo remove password from source control
-            password=(params and params.password) or "AVNS_nUSNiHJE3MlXGmj",
-            port=(params and params.port) or 25060)
-
         # self.conn = pg.connect(
         #     host=(
-        #         params and params.host) or "postgresql.joshkohut.com",
+        #         params and params.host) or "db-redbot-nyc3-42069-do-user-1692759-0.b.db.ondigitalocean.com",
         #     dbname=(params and params.dbname) or "pokemon_db",
         #     user=(params and params.user) or "redbot",
         #     # todo remove password from source control
-        #     password=(params and params.password) or "bfFLG9tUYPpW7272vzhX52",
-        #     port=(params and params.port) or 5432)
+        #     password=(params and params.password) or "AVNS_nUSNiHJE3MlXGmj",
+        #     port=(params and params.port) or 25060)
+
+        self.conn = pg.connect(
+            host=(
+                params and params.host) or "postgresql.joshkohut.com",
+            dbname=(params and params.dbname) or "pokemon_db",
+            user=(params and params.user) or "redbot",
+            # todo remove password from source control
+            password=(params and params.password) or "bfFLG9tUYPpW7272vzhX52",
+            port=(params and params.port) or 5432)
 
     def __del__(self):
         self.conn.close()
