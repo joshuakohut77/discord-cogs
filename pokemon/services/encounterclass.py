84a71b14966203bd26e19af3a273a07dfb1feba6
joshuakohut77 <joshuakohut77@gmail.com>
2024-12-27 22:22:36 -0500

    Update encounterclass.py

--- pokemon/services/encounterclass.py
+++ pokemon/services/encounterclass.py
@@ -8,7 +8,7 @@ from datetime import datetime
 import sys
 print(sys.path)
 
-from expclass import experiance as exp
+from .expclass import experiance as exp
 from inventoryclass import inventory as inv
 from leaderboardclass import leaderboard
 from loggerclass import logger as log
