a3fb6ccb35f4857fa0c8e82987bd15a8e262550b
joshuakohut77 <joshuakohut77@gmail.com>
2024-12-28 00:33:01 -0500

    Update trainerclass.py

--- pokemon/services/trainerclass.py
+++ pokemon/services/trainerclass.py
@@ -776,7 +776,7 @@ class trainer:
 
     def __checkCreateTrainer(self):
         """ this will check if a trainerId exists and if not, insert them into the database """
-        db = None
+        db = dbconn()
         try:
             db = dbconn()
             # do this check to see if trainer exists
