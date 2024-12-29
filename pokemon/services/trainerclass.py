ac84f282b6f53d06264eb3414be2cd6491eec8e6
joshuakohut77 <joshuakohut77@gmail.com>
2024-12-28 23:21:41 -0500

    debug buttons

--- pokemon/services/trainerclass.py
+++ pokemon/services/trainerclass.py
@@ -780,7 +780,7 @@ class trainer:
     def __checkCreateTrainer(self):
         """ this will check if a trainerId exists and if not, insert them into the database """
         db = dbconn()
-        print(db)
+        
         try:
             db = dbconn()
             # do this check to see if trainer exists
