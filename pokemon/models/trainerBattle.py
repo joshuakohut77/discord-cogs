edf43c7f07a417609aa6b188929bf31af8ea47fd
Josh Kohut <joshuakohut77@gmail.com>
2026-02-04 09:51:02 -0500

    Update trainerBattle.py

--- pokemon/models/trainerBattle.py
+++ pokemon/models/trainerBattle.py
@@ -5,7 +5,17 @@ class TrainerBattleModel:
     
     def __init__(self, results: json):
         self.name = results['name']
-        self.spritePath = results['spritePath']
+        
+        # Handle both 'spritePath' and 'filename' keys
+        # Some entries in enemyTrainers.json use 'filename', others use 'spritePath'
+        if 'spritePath' in results:
+            self.spritePath = results['spritePath']
+        elif 'filename' in results:
+            self.spritePath = results['filename']
+        else:
+            # Fallback - no sprite path provided
+            self.spritePath = None
+        
         self.money = results['money']
         self.enemy_uuid = results['enemy_uuid']
         self.pokemon = results['pokemon'] # this is a list of pokemon json in the format of { "pokemon": level }
