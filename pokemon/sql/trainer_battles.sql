5dd32ec5ac54a23c01140116840d14b43bff282c
joshuakohut77 <joshuakohut77@gmail.com>
2026-02-17 15:24:48 -0500

    Update trainer_battles.sql

--- pokemon/sql/trainer_battles.sql
+++ pokemon/sql/trainer_battles.sql
@@ -5,7 +5,7 @@
 CREATE TABLE IF NOT EXISTS public.trainer_battles
 (
     id bigint NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 9223372036854775807 CACHE 1 ),
-    discord_id character varying(20) COLLATE pg_catalog."default" NOT NULL,
+    discord_id character varying COLLATE pg_catalog."default" NOT NULL,
     "locationId" integer NOT NULL,
     enemy_uuid character varying(10) COLLATE pg_catalog."default" NOT NULL,
     CONSTRAINT trainer_battles_pkey PRIMARY KEY (id)
