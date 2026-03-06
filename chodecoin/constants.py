b64d655c04fe2d1e1151950f1f36ab414c375435
joshuakohut77 <joshuakohut77@gmail.com>
2026-03-06 16:05:29 -0500

    Update constants.py

--- chodecoin/constants.py
+++ chodecoin/constants.py
@@ -4,9 +4,9 @@
 # ---------------------------------------------------------------
 
 # Option 1:
-COIN_EMOJI = "<:Emoji_1:1479584171864232117>"
+# COIN_EMOJI = "<:Emoji_1:1479584171864232117>"
 
 # Option 2 (uncomment this and comment out Option 1 to swap):
-# COIN_EMOJI = "<:Emoji_2:1479584228390994142>"
+COIN_EMOJI = "<:Emoji_2:1479584228390994142>"
 
 EMBED_COLOR = 0xFFD700  # gold
